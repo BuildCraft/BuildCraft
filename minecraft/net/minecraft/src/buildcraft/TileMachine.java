dcfa88a9e9021844b4edb00bae4eb394d497510c
SpaceToad <spacetoadcraft@gmail.com>
2011-05-02 21:39:36 +0000

    iron pipes now react to power change, for #24

--- minecraft/net/minecraft/src/buildcraft/TileMachine.java
+++ minecraft/net/minecraft/src/buildcraft/TileMachine.java
@@ -219,8 +219,6 @@ public class TileMachine extends TileEntity implements IArmListener {
 				ItemStack items = new ItemStack(item, 1);
 
 				// First, try to add to a nearby chest
-				
-				System.out.println ("ADD TO RANDOM INVENTORY");
 
 				added = Utils.addToRandomInventory(this, Orientations.Unknown,
 						items);
