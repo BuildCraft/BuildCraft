19563f01995a5bf7742dca94a4305292e4112dd5
SpaceToad <spacetoadcraft@gmail.com>
2011-05-04 19:34:20 +0000

    moved code around

--- minecraft/net/minecraft/src/buildcraft/IPipeEntry.java
+++
@@ -1,8 +0,0 @@
-package net.minecraft.src.buildcraft;
-
-public interface IPipeEntry {
-	
-	public void entityEntering (EntityPassiveItem item, Orientations orientation);
-	
-	public Position getPosition ();
-}
--- minecraft/net/minecraft/src/buildcraft/ITickListener.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.src.buildcraft;
-
-import net.minecraft.client.Minecraft;
-
-public interface ITickListener {
-	
-	public void tick (Minecraft minecraft);
-
-}
