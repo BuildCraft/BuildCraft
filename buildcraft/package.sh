d2de7c9b5c4798a90274286d65d6674e5ded5641
spacetoad <spacetoad>
2011-05-20 21:31:57 +0000

    update packaging for builders mod

--- buildcraft/package.sh
+++ buildcraft/package.sh
@@ -28,14 +28,14 @@ dir=../../release-$version
 rm -rf $dir
 mkdir $dir
 
-zip -r $dir/buildcraft-core-$version.zip mod_BuildCraftCore* BuildCraftCore.class RenderPassiveItem.class BuildCraftBlockUtil.class buildcraft/core net/minecraft/src/buildcraft/core
+zip -r $dir/buildcraft-A-core-$version.zip mod_BuildCraftCore* BuildCraftCore.class RenderPassiveItem.class BuildCraftBlockUtil.class buildcraft/core net/minecraft/src/buildcraft/core
 
-zip -r $dir/buildcraft-transport-$version.zip mod_BuildCraftTransport.class BuildCraftTransport.class buildcraft/transport net/minecraft/src/buildcraft/transport
+zip -r $dir/buildcraft-B-transport-$version.zip mod_BuildCraftTransport.class BuildCraftTransport.class buildcraft/transport net/minecraft/src/buildcraft/transport
 
-zip -r $dir/buildcraft-factory-$version.zip mod_BuildCraftFactory.class buildcraft/factory net/minecraft/src/buildcraft/factory
+zip -r $dir/buildcraft-C-factory-$version.zip mod_BuildCraftFactory.class buildcraft/factory net/minecraft/src/buildcraft/factory
 
-zip -r $dir/buildcraft-buiders-$version.zip mod_BuildCraftBuilders.class buildcraft/builders net/minecraft/src/buildcraft/builders
+zip -r $dir/buildcraft-B-builders-$version.zip mod_BuildCraftBuilders.class buildcraft/builders net/minecraft/src/buildcraft/builders
 
-zip -r $dir/buildcraft-devel-$version.zip mod_BuildCraftDevel.class buildcraft/devel
+zip -r $dir/buildcraft-D-devel-$version.zip mod_BuildCraftDevel.class buildcraft/devel
 
 
