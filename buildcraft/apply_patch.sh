203aed1020b597f46006191563f87bba9e3ebe12
SpaceToad <spacetoadcraft@gmail.com>
2011-06-02 21:08:04 +0000

    fixed patch application, for #22

--- buildcraft/apply_patch.sh
+++ buildcraft/apply_patch.sh
@@ -1,6 +1,8 @@
 if [ ! -f ../src_base ]; then
    cp -r ../src ../src_base
    patch -p0 < minecraft.patch
+   cd ../src/minecraft_server/net/minecraft/src
+   unix2dos *
 else
    echo "patch already applied - remove src and src_base and decompile again"
 fi
