123ef43aa736ec64f968cb7f8922fe60bb771d7e
SpaceToad <spacetoadcraft@gmail.com>
2011-06-03 00:06:48 +0000

    fixed warnings

--- buildcraft/common/net/minecraft/src/buildcraft/transport/BlockPipe.java
+++ buildcraft/common/net/minecraft/src/buildcraft/transport/BlockPipe.java
@@ -13,7 +13,6 @@ import net.minecraft.src.buildcraft.api.IBlockPipe;
 import net.minecraft.src.buildcraft.api.IPipeConnection;
 import net.minecraft.src.buildcraft.api.IPipeEntry;
 import net.minecraft.src.buildcraft.api.Orientations;
-import net.minecraft.src.buildcraft.core.CoreProxy;
 import net.minecraft.src.buildcraft.core.IMachine;
 import net.minecraft.src.buildcraft.core.Utils;
 
