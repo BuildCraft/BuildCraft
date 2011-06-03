0b865e856d4cca7f463ae0a92f62bb5a5476846e
spacetoad <spacetoad>
2011-06-03 00:00:48 +0000

    made progress in SMP implementation, for #22

--- buildcraft/buildcraft_client/src/net/minecraft/src/buildcraft/transport/GuiFilter.java
+++ buildcraft/buildcraft_client/src/net/minecraft/src/buildcraft/transport/GuiFilter.java
@@ -6,46 +6,13 @@ import net.minecraft.src.Container;
 import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.GuiContainer;
 import net.minecraft.src.IInventory;
+import net.minecraft.src.ItemStack;
 import net.minecraft.src.Slot;
 
 public class GuiFilter extends GuiContainer {
 	
 	IInventory playerInventory;
 	IInventory filterInventory;
-	
-	static class CraftingInv extends Container {
-		
-		public CraftingInv (IInventory playerInventory, IInventory filterInventory) {
-			for(int k = 0; k < 6; k++)
-	        {
-	            for(int j1 = 0; j1 < 9; j1++)
-	            {
-	                addSlot(new Slot(filterInventory, j1 + k * 9, 8 + j1 * 18, 18 + k * 18));
-	            }
-
-	        }
-
-	        for(int l = 0; l < 3; l++)
-	        {
-	            for(int k1 = 0; k1 < 9; k1++)
-	            {
-	                addSlot(new Slot(playerInventory, k1 + l * 9 + 9, 8 + k1 * 18, 140 + l * 18));
-	            }
-
-	        }
-
-	        for(int i1 = 0; i1 < 9; i1++)
-	        {
-	            addSlot(new Slot(playerInventory, i1, 8 + i1 * 18, 198));
-	        }
-		}
-
-		@Override
-		public boolean isUsableByPlayer(EntityPlayer entityplayer) {
-			return true;
-		}
-		
-	}
 	
 	public GuiFilter(IInventory playerInventory, IInventory filterInventory) {
 		super(new CraftingInv(playerInventory, filterInventory));
--- buildcraft/common/net/minecraft/src/buildcraft/transport/BlockDiamondPipe.java
+++ buildcraft/common/net/minecraft/src/buildcraft/transport/BlockDiamondPipe.java
@@ -5,6 +5,7 @@ import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.Material;
 import net.minecraft.src.TileEntity;
 import net.minecraft.src.World;
+import net.minecraft.src.buildcraft.api.APIProxy;
 import net.minecraft.src.buildcraft.api.Orientations;
 
 public class BlockDiamondPipe extends BlockPipe {
@@ -24,9 +25,12 @@ public class BlockDiamondPipe extends BlockPipe {
 	public boolean blockActivated(World world, int i, int j, int k, EntityPlayer entityplayer) {		
 		TileDiamondPipe	tileRooter = (TileDiamondPipe) world.getBlockTileEntity(i, j, k);
 		
-		TransportProxy.displayGUIFilter(entityplayer, tileRooter);
-		
-		return true;
+		if (APIProxy.isClient(world)) {
+			return true;
+		} else {
+			TransportProxy.displayGUIFilter(entityplayer, tileRooter);
+			return true;
+		}		
 	}	    
 	
     public int getTextureForConnection (Orientations connection, int metadata) {
