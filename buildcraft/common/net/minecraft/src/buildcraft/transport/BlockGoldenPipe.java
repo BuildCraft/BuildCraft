0cfde99d1a03b58c4984f7202714a99cc93a093d
spacetoad <spacetoad>
2011-06-15 20:21:00 +0000

    modified golden pipe texture when accelerating, for #130

--- buildcraft/common/net/minecraft/src/buildcraft/transport/BlockGoldenPipe.java
+++ buildcraft/common/net/minecraft/src/buildcraft/transport/BlockGoldenPipe.java
@@ -1,21 +1,48 @@
 package net.minecraft.src.buildcraft.transport;
 
+import net.minecraft.src.ChunkCache;
+import net.minecraft.src.IBlockAccess;
 import net.minecraft.src.Material;
 import net.minecraft.src.TileEntity;
 
 public class BlockGoldenPipe extends BlockPipe {
 	
 	
+	private int inactiveTexture;
+	private int activeTexture;
+
 	public BlockGoldenPipe(int i) {
 		super(i, Material.iron);
 
-		blockIndexInTexture = 1 * 16 + 4;
+		inactiveTexture = 1 * 16 + 4;
+		activeTexture = 1 * 16 + 14;
 	}
 	
 	@Override
 	protected TileEntity getBlockEntity() {
 		return new TileGoldenPipe ();
 	}
+	
+    public int getBlockTexture(IBlockAccess iblockaccess, int i, int j, int k, int l)
+    {
+    	boolean isPowered = false;
+		
+		if (iblockaccess instanceof ChunkCache) {
+			isPowered = ((ChunkCache) iblockaccess).getBlockTileEntity(i, j, k).worldObj
+					.isBlockIndirectlyGettingPowered(i, j, k);
+		}
+		
+		if (isPowered) {
+			return activeTexture;
+		} else {
+			return inactiveTexture;
+		}
+    }
+
+    public int getBlockTextureFromSideAndMetadata(int i, int j)
+    {
+        return inactiveTexture;
+    }
 
 	
 }
