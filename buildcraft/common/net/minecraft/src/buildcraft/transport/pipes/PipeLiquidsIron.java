a45f56e97c01ab8401235fc77cea8337f6c14ebc
SpaceToad <spacetoadcraft@gmail.com>
2011-09-10 20:16:36 +0000

    added missing files

---
+++ buildcraft/common/net/minecraft/src/buildcraft/transport/pipes/PipeLiquidsIron.java
@@ -0,0 +1,38 @@
+package net.minecraft.src.buildcraft.transport.pipes;
+
+import net.minecraft.src.buildcraft.api.Orientations;
+import net.minecraft.src.buildcraft.transport.Pipe;
+import net.minecraft.src.buildcraft.transport.PipeLogicIron;
+import net.minecraft.src.buildcraft.transport.PipeTransportLiquids;
+
+public class PipeLiquidsIron extends Pipe {
+
+	private int baseTexture = 7 * 16 + 3;
+	private int plainTexture = 1 * 16 + 3;
+	private int nextTexture = baseTexture;
+
+	public PipeLiquidsIron(int itemID) {
+		super(new PipeTransportLiquids(), new PipeLogicIron(), itemID);
+	}
+
+	@Override
+	public void prepareTextureFor(Orientations connection) {
+		if (connection == Orientations.Unknown) {
+			nextTexture = baseTexture;
+		} else {
+			int metadata = worldObj.getBlockMetadata(xCoord, yCoord, zCoord);
+
+			if (metadata == connection.ordinal()) {
+				nextTexture = baseTexture;
+			} else {
+				nextTexture = plainTexture;
+			}
+		}
+	}
+	
+	@Override
+	public int getBlockTexture() {
+		return nextTexture;
+	}
+	
+}
