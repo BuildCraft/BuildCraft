5b687aca1c508c0ff5ec4e4ac6679fe57c885388
spacetoad <spacetoad>
2011-09-17 11:09:13 +0000

    finished loading of legacy pipes, for #219

--- buildcraft/common/net/minecraft/src/buildcraft/transport/pipes/PipeItemsIron.java
+++ buildcraft/common/net/minecraft/src/buildcraft/transport/pipes/PipeItemsIron.java
@@ -1,14 +1,38 @@
 package net.minecraft.src.buildcraft.transport.pipes;
 
+import net.minecraft.src.buildcraft.api.Orientations;
 import net.minecraft.src.buildcraft.transport.Pipe;
 import net.minecraft.src.buildcraft.transport.PipeLogicIron;
 import net.minecraft.src.buildcraft.transport.PipeTransportItems;
 
 public class PipeItemsIron extends Pipe {
+	
+	private int baseTexture = 1 * 16 + 2;
+	private int plainTexture = 1 * 16 + 3;
+	private int nextTexture = baseTexture;
 
 	public PipeItemsIron(int itemID) {
 		super(new PipeTransportItems(), new PipeLogicIron(), itemID);
-		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void prepareTextureFor(Orientations connection) {
+		if (connection == Orientations.Unknown) {
+			nextTexture = baseTexture;
+		} else {
+			int metadata = worldObj.getBlockMetadata(xCoord, yCoord, zCoord);
+
+			if (metadata == connection.ordinal()) {
+				nextTexture = baseTexture;
+			} else {
+				nextTexture = plainTexture;
+			}
+		}
+	}
+	
+	@Override
+	public int getBlockTexture() {
+		return nextTexture;
 	}
 
 }
