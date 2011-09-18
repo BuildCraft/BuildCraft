82434e2dc4728e7f6c4eb0101fb72a7f5b7cd3f6
spacetoad <spacetoad>
2011-09-18 13:13:52 +0000

    fixed collisions for frame, for #227

--- buildcraft/common/net/minecraft/src/buildcraft/factory/BlockFrame.java
+++ buildcraft/common/net/minecraft/src/buildcraft/factory/BlockFrame.java
@@ -1,5 +1,6 @@
 package net.minecraft.src.buildcraft.factory;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import net.minecraft.src.AxisAlignedBB;
@@ -7,6 +8,8 @@ import net.minecraft.src.Block;
 import net.minecraft.src.BuildCraftCore;
 import net.minecraft.src.IBlockAccess;
 import net.minecraft.src.Material;
+import net.minecraft.src.MovingObjectPosition;
+import net.minecraft.src.Vec3D;
 import net.minecraft.src.World;
 import net.minecraft.src.buildcraft.api.IBlockPipe;
 import net.minecraft.src.buildcraft.api.IPipeConnection;
@@ -14,93 +17,180 @@ import net.minecraft.src.buildcraft.api.Orientations;
 import net.minecraft.src.buildcraft.core.Utils;
 import net.minecraft.src.forge.ITextureProvider;
 
-public class BlockFrame extends Block implements IPipeConnection, IBlockPipe, ITextureProvider {	
-	
+public class BlockFrame extends Block implements IPipeConnection, IBlockPipe,
+		ITextureProvider {
+
 	public BlockFrame(int i) {
 		super(i, Material.glass);
-		
-		blockIndexInTexture = 16 * 2 + 2; 
-		
+
+		blockIndexInTexture = 16 * 2 + 2;
+
 		setHardness(0.5F);
 	}
-    
-    public boolean isOpaqueCube()
-    {
-        return false;
-    }
-    
-    public boolean renderAsNormalBlock()
-    {
-        return false;
-    }
-    
-    public boolean isACube () {
-    	return false;
-    }    
-    
-    public int idDropped(int i, Random random)
-    {
-        return -1;
-    }
-    
-    public int getRenderType()
-    {
-        return BuildCraftCore.pipeModel;
-    }
-    
-    public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int i, int j, int k)
-    {
-   		float xMin = Utils.pipeMinPos, xMax = Utils.pipeMaxPos, 
-   		yMin = Utils.pipeMinPos, yMax = Utils.pipeMaxPos, 
-   		zMin = Utils.pipeMinPos, zMax = Utils.pipeMaxPos;
 
-   		if (Utils.checkPipesConnections(world, i, j, k, i - 1, j, k)) {
-   			xMin = 0.0F;
-   		}
+	public boolean isOpaqueCube() {
+		return false;
+	}
 
-   		if (Utils.checkPipesConnections(world, i, j, k, i + 1, j, k)) {
-   			xMax = 1.0F;
-   		}
+	public boolean renderAsNormalBlock() {
+		return false;
+	}
 
-   		if (Utils.checkPipesConnections(world, i, j, k, i, j - 1, k)) {
-   			yMin = 0.0F;
-   		}
+	public boolean isACube() {
+		return false;
+	}
 
-   		if (Utils.checkPipesConnections(world, i, j, k, i, j + 1, k)) {
-   			yMax = 1.0F;
-   		}
+	public int idDropped(int i, Random random) {
+		return -1;
+	}
 
-   		if (Utils.checkPipesConnections(world, i, j, k, i, j, k - 1)) {
-   			zMin = 0.0F;
-   		}
+	public int getRenderType() {
+		return BuildCraftCore.pipeModel;
+	}
 
-   		if (Utils.checkPipesConnections(world, i, j, k, i, j, k + 1)) {
-   			zMax = 1.0F;
-   		}
-       	
-       	    
-   		return AxisAlignedBB.getBoundingBoxFromPool((double) i + xMin,
-   				(double) j + yMin, (double) k + zMin, (double) i + xMax,
-   				(double) j + yMax, (double) k + zMax);
-       }
-   	
-       public AxisAlignedBB getSelectedBoundingBoxFromPool(World world, int i, int j, int k)
-       {
-           return getCollisionBoundingBoxFromPool (world, i, j, k);
-       }
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int i,
+			int j, int k) {
+		float xMin = Utils.pipeMinPos, xMax = Utils.pipeMaxPos, yMin = Utils.pipeMinPos, yMax = Utils.pipeMaxPos, zMin = Utils.pipeMinPos, zMax = Utils.pipeMaxPos;
+
+		if (Utils.checkPipesConnections(world, i, j, k, i - 1, j, k)) {
+			xMin = 0.0F;
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i + 1, j, k)) {
+			xMax = 1.0F;
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i, j - 1, k)) {
+			yMin = 0.0F;
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i, j + 1, k)) {
+			yMax = 1.0F;
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i, j, k - 1)) {
+			zMin = 0.0F;
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i, j, k + 1)) {
+			zMax = 1.0F;
+		}
+
+		return AxisAlignedBB.getBoundingBoxFromPool((double) i + xMin,
+				(double) j + yMin, (double) k + zMin, (double) i + xMax,
+				(double) j + yMax, (double) k + zMax);
+	}
+
+	public AxisAlignedBB getSelectedBoundingBoxFromPool(World world, int i,
+			int j, int k) {
+		return getCollisionBoundingBoxFromPool(world, i, j, k);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Override
+	public void getCollidingBoundingBoxes(World world, int i, int j, int k,
+			AxisAlignedBB axisalignedbb, ArrayList arraylist) {
+		setBlockBounds(Utils.pipeMinPos, Utils.pipeMinPos, Utils.pipeMinPos,
+				Utils.pipeMaxPos, Utils.pipeMaxPos, Utils.pipeMaxPos);
+		super.getCollidingBoundingBoxes(world, i, j, k, axisalignedbb,
+				arraylist);
+
+		if (Utils.checkPipesConnections(world, i, j, k, i - 1, j, k)) {
+			setBlockBounds(0.0F, Utils.pipeMinPos, Utils.pipeMinPos,
+					Utils.pipeMaxPos, Utils.pipeMaxPos, Utils.pipeMaxPos);
+			super.getCollidingBoundingBoxes(world, i, j, k, axisalignedbb,
+					arraylist);
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i + 1, j, k)) {
+			setBlockBounds(Utils.pipeMinPos, Utils.pipeMinPos,
+					Utils.pipeMinPos, 1.0F, Utils.pipeMaxPos, Utils.pipeMaxPos);
+			super.getCollidingBoundingBoxes(world, i, j, k, axisalignedbb,
+					arraylist);
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i, j - 1, k)) {
+			setBlockBounds(Utils.pipeMinPos, 0.0F, Utils.pipeMinPos,
+					Utils.pipeMaxPos, Utils.pipeMaxPos, Utils.pipeMaxPos);
+			super.getCollidingBoundingBoxes(world, i, j, k, axisalignedbb,
+					arraylist);
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i, j + 1, k)) {
+			setBlockBounds(Utils.pipeMinPos, Utils.pipeMinPos,
+					Utils.pipeMinPos, Utils.pipeMaxPos, 1.0F, Utils.pipeMaxPos);
+			super.getCollidingBoundingBoxes(world, i, j, k, axisalignedbb,
+					arraylist);
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i, j, k - 1)) {
+			setBlockBounds(Utils.pipeMinPos, Utils.pipeMinPos, 0.0F,
+					Utils.pipeMaxPos, Utils.pipeMaxPos, Utils.pipeMaxPos);
+			super.getCollidingBoundingBoxes(world, i, j, k, axisalignedbb,
+					arraylist);
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i, j, k + 1)) {
+			setBlockBounds(Utils.pipeMinPos, Utils.pipeMinPos,
+					Utils.pipeMinPos, Utils.pipeMaxPos, Utils.pipeMaxPos, 1.0F);
+			super.getCollidingBoundingBoxes(world, i, j, k, axisalignedbb,
+					arraylist);
+		}
+
+		setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
+	}
+	
+	@Override
+	public MovingObjectPosition collisionRayTrace(World world, int i, int j,
+			int k, Vec3D vec3d, Vec3D vec3d1) {
+		float xMin = Utils.pipeMinPos, xMax = Utils.pipeMaxPos, yMin = Utils.pipeMinPos, yMax = Utils.pipeMaxPos, zMin = Utils.pipeMinPos, zMax = Utils.pipeMaxPos;
+
+		if (Utils.checkPipesConnections(world, i, j, k, i - 1, j, k)) {
+			xMin = 0.0F;
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i + 1, j, k)) {
+			xMax = 1.0F;
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i, j - 1, k)) {
+			yMin = 0.0F;
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i, j + 1, k)) {
+			yMax = 1.0F;
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i, j, k - 1)) {
+			zMin = 0.0F;
+		}
+
+		if (Utils.checkPipesConnections(world, i, j, k, i, j, k + 1)) {
+			zMax = 1.0F;
+		}
+
+		setBlockBounds(xMin, yMin, zMin, xMax, yMax, zMax);
+
+		MovingObjectPosition r = super.collisionRayTrace(world, i, j, k, vec3d,
+				vec3d1);
+
+		setBlockBounds(0, 0, 0, 1, 1, 1);
+
+		return r;
+	}
 
 	@Override
 	public boolean isPipeConnected(IBlockAccess blockAccess, int x1, int y1,
 			int z1, int x2, int y2, int z2) {
 		return blockAccess.getBlockId(x2, y2, z2) == blockID;
 	}
-	
-    public float getHeightInPipe () {
-    	return 0.5F;
-    }    
-    
-    @Override
-	public String getTextureFile() {	
+
+	public float getHeightInPipe() {
+		return 0.5F;
+	}
+
+	@Override
+	public String getTextureFile() {
 		return BuildCraftCore.customBuildCraftTexture;
 	}
 
@@ -108,6 +198,6 @@ public class BlockFrame extends Block implements IPipeConnection, IBlockPipe, IT
 	public void prepareTextureFor(IBlockAccess blockAccess, int i, int j,
 			int k, Orientations connection) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
