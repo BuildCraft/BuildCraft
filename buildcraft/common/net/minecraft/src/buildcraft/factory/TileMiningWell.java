aff8ae1d886c81b278e8be19370e6db6998d4889
SpaceToad <spacetoadcraft@gmail.com>
2011-09-30 17:50:49 +0000

    fixed oil id computation (#229) and added license notice

--- buildcraft/common/net/minecraft/src/buildcraft/factory/TileMiningWell.java
+++ buildcraft/common/net/minecraft/src/buildcraft/factory/TileMiningWell.java
@@ -1,3 +1,11 @@
+/** 
+ * BuildCraft is open-source. It is distributed under the terms of the 
+ * BuildCraft Open Source License. It grants rights to read, modify, compile
+ * or run the code. It does *NOT* grant the right to redistribute this software
+ * or its modifications in any form, binary or source, except if expressively
+ * granted by the copyright holder.
+ */
+
 package net.minecraft.src.buildcraft.factory;
 
 import net.minecraft.src.Block;
@@ -12,7 +20,6 @@ import net.minecraft.src.buildcraft.api.Orientations;
 import net.minecraft.src.buildcraft.api.PowerProvider;
 import net.minecraft.src.buildcraft.core.IMachine;
 import net.minecraft.src.buildcraft.core.StackUtil;
-import net.minecraft.src.buildcraft.core.TileBuildCraft;
 import net.minecraft.src.buildcraft.core.Utils;
 
 public class TileMiningWell extends TileMachine implements IMachine, IPowerReceptor {
