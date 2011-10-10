d6b0df74d8e354c0218d9567ac34d8e4c9165376
spacetoad <spacetoad>
2011-10-10 21:59:54 +0000

    improved copyright notice of the API

--- buildcraft/common/net/minecraft/src/buildcraft/api/TileNetworkData.java
+++ buildcraft/common/net/minecraft/src/buildcraft/api/TileNetworkData.java
@@ -1,9 +1,10 @@
 /** 
- * BuildCraft is open-source. It is distributed under the terms of the 
- * BuildCraft Open Source License. It grants rights to read, modify, compile
- * or run the code. It does *NOT* grant the right to redistribute this software
- * or its modifications in any form, binary or source, except if expressively
- * granted by the copyright holder.
+ * Copyright (c) SpaceToad, 2011
+ * 
+ * This file is part of the BuildCraft API. You have the rights to read, 
+ * modify, compile or run this the code without restrictions. In addition, it
+ * allowed to redistribute this API as well, either in source or binaries 
+ * form, or to integrate it into an other mod.
  */
 
 package net.minecraft.src.buildcraft.api;
