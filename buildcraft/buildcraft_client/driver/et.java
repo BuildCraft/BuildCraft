d9d24170e37c9459f9e084f8577e0ca6d36d96d6
SpaceToad <spacetoadcraft@gmail.com>
2011-10-19 20:58:42 +0000

    added driver for Client

---
+++ buildcraft/buildcraft_client/driver/et.java
@@ -0,0 +1,9 @@
+// this is needed for the sound engine to work with deobfuscated sourcecode without crashing
+
+public class et {
+    public static int b(double d)
+    {
+        int i = (int)d;
+        return d >= (double)i ? i : i - 1;
+    }
+}
