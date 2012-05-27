999d26eb2376352b74a2d3f0ec1660f80644ddf5
SirSengir <me@sengir.net>
2012-05-27 11:26:27 +0200

    Fixed previous commit.

--- common/net/minecraft/src/buildcraft/transport/BlockGenericPipe.java
+++ common/net/minecraft/src/buildcraft/transport/BlockGenericPipe.java
@@ -328,8 +328,7 @@ public class BlockGenericPipe extends BlockContainer implements
 					(entityplayer.getCurrentEquippedItem() == null
 					|| !(entityplayer.getCurrentEquippedItem().getItem() instanceof IToolWrench))) {
 
-				if(!APIProxy.isRemote()
-						&& (pipe.hasGate() || pipe.isWired()))
+				if(pipe.hasGate() || pipe.isWired())
 					return stripEquipment(pipe);
 
 			} else if (entityplayer.getCurrentEquippedItem() == null) {
@@ -401,7 +400,8 @@ public class BlockGenericPipe extends BlockContainer implements
 		// Try to strip wires first, starting with yellow.
 		for(IPipe.WireColor color : IPipe.WireColor.values())
 			if(pipe.wireSet[color.reverse().ordinal()]) {
-				dropWire(color.reverse(), pipe.worldObj, pipe.xCoord, pipe.yCoord, pipe.zCoord);
+				if(!APIProxy.isRemote())
+					dropWire(color.reverse(), pipe.worldObj, pipe.xCoord, pipe.yCoord, pipe.zCoord);
 				pipe.wireSet[color.reverse().ordinal()] = false;
 				pipe.worldObj.markBlockNeedsUpdate(pipe.xCoord, pipe.yCoord, pipe.zCoord);
 				return true;
@@ -409,7 +409,8 @@ public class BlockGenericPipe extends BlockContainer implements
 
 		// Try to strip gate next
 		if(pipe.hasGate()) {
-			pipe.gate.dropGate(pipe.worldObj, pipe.xCoord, pipe.yCoord, pipe.zCoord);
+			if(!APIProxy.isRemote())
+				pipe.gate.dropGate(pipe.worldObj, pipe.xCoord, pipe.yCoord, pipe.zCoord);
 			pipe.resetGate();
 			return true;
 		}
