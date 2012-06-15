1f25b8a457757f6607561478bd3aa1fc151d35fd
Kyprus <zachl44@gmail.com>
2012-06-14 20:57:17 -0400

    Fixed a unicode char.

--- buildcraft_client/net/minecraft/src/buildcraft/energy/GuiEngine.java
+++ buildcraft_client/net/minecraft/src/buildcraft/energy/GuiEngine.java
@@ -39,7 +39,7 @@ public abstract class GuiEngine extends GuiBuildCraft {
 			fontRenderer.drawStringWithShadow(StringUtil.localize("gui.stored") + ":", x + 22, y + 44, subheaderColour);
 			fontRenderer.drawString(engine.getEnergyStored() + " MJ", x + 22, y + 56, textColour);
 			fontRenderer.drawStringWithShadow(StringUtil.localize("gui.heat") + ":", x + 22, y + 68, subheaderColour);
-			fontRenderer.drawString(((double) engine.getHeat() / (double) 10) + " °C", x + 22, y + 80, textColour);
+			fontRenderer.drawString(((double) engine.getHeat() / (double) 10) + " \u00B0C", x + 22, y + 80, textColour);
 
 		}
 
