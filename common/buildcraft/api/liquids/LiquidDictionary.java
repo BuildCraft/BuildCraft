22c4ad532b90e76862003daa7216945645a2a9d8
CovertJaguar <CovertJaguar@railcraft.com>
2012-08-02 06:01:53 -0700

    Added Javadocs

--- common/buildcraft/api/liquids/LiquidDictionary.java
+++ common/buildcraft/api/liquids/LiquidDictionary.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * When creating liquids you should register them with this class.
  *
  * @author CovertJaguar <railcraft.wikispaces.com>
  */
@@ -12,6 +13,17 @@ public abstract class LiquidDictionary
 
     private static Map<String, LiquidStack> liquids = new HashMap<String, LiquidStack>();
 
+    /**
+     * When creating liquids you should call this function.
+     *
+     * Upon passing it a name and liquid item it will return either
+     * a preexisting implementation of that liquid or the liquid passed in.
+     *
+     *
+     * @param name the name of the liquid
+     * @param liquid the liquid to use if one doesn't exist
+     * @return
+     */
     public static LiquidStack getOrCreateLiquid(String name, LiquidStack liquid)
     {
         LiquidStack existing = liquids.get(name);
