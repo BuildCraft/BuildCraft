eac30bea264af42b66e66e4df6e1845287cc0f9b
SirSengir <me@sengir.net>
2012-09-02 20:01:35 +0200

    Fixed broken version.

--- common/buildcraft/core/DefaultProps.java
+++ common/buildcraft/core/DefaultProps.java
@@ -11,7 +11,7 @@ package buildcraft.core;
 
 public class DefaultProps {
 
-	public static final String VERSION = "4";
+	public static final String VERSION = "@VERSION@";
 
 	public static final String DEPENDENCY_CORE = "required-after:BuildCraft|Core@"+VERSION;
 	public static final String DEPENDENCY_TRANSPORT = "required-after:BuildCraft|Transport@"+VERSION;
