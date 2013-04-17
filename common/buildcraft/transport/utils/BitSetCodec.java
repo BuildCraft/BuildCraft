62a22420121331af2fc81dafca01337423e8c076
Krapht <kraphteu@gmail.com>
2013-04-17 19:29:32 +0200

    Change ConnectionMatrix to use a BitSet internally to represent the connections. Saves 25 bytes per packet

--- common/buildcraft/transport/utils/BitSetCodec.java
+++ common/buildcraft/transport/utils/BitSetCodec.java
@@ -5,18 +5,18 @@ import java.util.BitSet;
 public class BitSetCodec {
 	public byte encode(BitSet set){
 		byte result = 0;
-		for (byte i = 0; i < set.length() && i < 8; i++ ){
-			if (set.get(i)){
-				result |= 0x1 << i;	
-			}
+		for (byte i = 0; i < 8; i++ ){
+			result <<= 1; 
+			result |= set.get(i) ? 1 : 0;
 		}
 		return result;
 	}
 	
 	public void decode(byte data, BitSet target){
 		target.clear();
-		for (byte i = 0; i < target.length() && i < 8; i++){
-			target.set(i, (data & 0x1 >>> i) > 0);
+		for (byte i = 0; i < 8; i++){
+			target.set(7-i, (data & 1) != 0); 
+			data >>= 1;
 		}
 	}
 }
