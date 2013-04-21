ff110f60683318fce923dfaff04c38d4828b09be
Krapht <kraphteu@gmail.com>
2013-04-21 15:43:23 +0200

    Fixed naming

--- common/buildcraft/energy/BlockEngine.java
+++ common/buildcraft/energy/BlockEngine.java
@@ -197,7 +197,7 @@ public class BlockEngine extends BlockContainer {
 	
 	@Override
 	@SideOnly(Side.CLIENT)
-	public Icon getBlockTextureFromSideAndMetadata(int par1, int par2)
+	public Icon getIcon(int par1, int par2)
 	{
 		switch (par2)
 		{
