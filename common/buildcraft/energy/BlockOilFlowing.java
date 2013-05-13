8d98cba857d6690a6a41952d3e87945f57f6461a
viliml <vilim.lendvaj@sk.t-com.hr>
2013-05-13 20:42:15 +0200

    Make flowing oil flammable and prone to explode(like real oil)

--- common/buildcraft/energy/BlockOilFlowing.java
+++ common/buildcraft/energy/BlockOilFlowing.java
@@ -284,4 +284,20 @@ public class BlockOilFlowing extends BlockFlowing implements ILiquid {
 	public void registerIcons(IconRegister iconRegister) {
 		this.theIcon = new Icon[]{iconRegister.registerIcon("buildcraft:oil"), iconRegister.registerIcon("buildcraft:oil_flow")};
 	}
+	
+	@Override
+	public void initializeBlock()
+	{
+		setBurnProperties(blockID, 100, 100);
+	}
+	
+	@Override
+	public void onNeighborBlockChange(World World, int x, int y, int z, int id)
+	{
+		if (id == Block.fire.blockID ||
+			id == Block.lavaStill.blockID || id == Block.lavaMoving.blockID)
+		{
+			World.createExplosion(null, x, y, z, 10, true);
+		}
+	}
 }
