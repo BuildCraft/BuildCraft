7aede827f108aa71eb5152481a124d7d0594a6e3
stranger1029 <highseeker@mail.com>
2013-05-31 10:56:51 +0900

    Fixed invalid energy loss in conductive pipe.

--- common/buildcraft/transport/PipeTransportPower.java
+++ common/buildcraft/transport/PipeTransportPower.java
@@ -229,9 +229,16 @@ public class PipeTransportPower extends PipeTransport {
 			double[] next = Arrays.copyOf(internalPower, 6);
 			internalPower = internalNextPower;
 			internalNextPower = next;
-			for (int i = 0; i < nextPowerQuery.length; i++) {
-				if (powerQuery[i] == 0.0d && internalNextPower[i] > 0) {
+			for (int i = 0; i < powerQuery.length; i++) {
+				int sum = 0;
+				for (int j = 0; j < powerQuery.length; j++) {
+					if (i != j)
+						sum += powerQuery[j];
+				}
+				if (sum == 0 && internalNextPower[i] > 0) {
 					internalNextPower[i] -= 1;
+					if (internalNextPower[i] < 0)
+						internalNextPower[i] = 0;
 				}
 			}
 		}
