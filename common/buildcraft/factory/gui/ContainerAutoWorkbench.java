2b0588fe5f7ea32e8e6babd25d6fb354a991333d
CovertJaguar <CovertJaguar@gmail.com>
2013-06-10 17:11:42 -0700

    Fixed minor GUI issue with AWB

--- common/buildcraft/factory/gui/ContainerAutoWorkbench.java
+++ common/buildcraft/factory/gui/ContainerAutoWorkbench.java
@@ -17,6 +17,7 @@ import buildcraft.core.gui.BuildCraftContainer;
 import buildcraft.core.gui.slots.SlotOutput;
 import buildcraft.core.gui.slots.SlotUntouchable;
 import buildcraft.core.gui.slots.SlotWorkbench;
+import buildcraft.core.proxy.CoreProxy;
 import buildcraft.core.utils.StringUtils;
 import buildcraft.core.utils.Utils;
 import buildcraft.factory.TileAutoWorkbench;
@@ -33,7 +34,15 @@ public class ContainerAutoWorkbench extends BuildCraftContainer {
 	public ContainerAutoWorkbench(InventoryPlayer inventoryplayer, TileAutoWorkbench t) {
 		super(t.getSizeInventory());
 
-		craftResult = new InventoryCraftResult();
+		craftResult = new InventoryCraftResult() {
+			@Override
+			public void setInventorySlotContents(int slot, ItemStack stack) {
+				super.setInventorySlotContents(slot, stack);
+				if (stack != null && tile.isLast() && CoreProxy.proxy.isRenderWorld(tile.worldObj)) {
+					Utils.addItemToolTip(stack, "tip", EnumChatFormatting.YELLOW + StringUtils.localize("gui.clickcraft"));
+				}
+			}
+		};
 		this.tile = t;
 		addSlotToContainer(new SlotUntouchable(craftResult, 0, 93, 27) {
 			@Override
@@ -100,9 +109,6 @@ public class ContainerAutoWorkbench extends BuildCraftContainer {
 	public final void onCraftMatrixChanged(IInventory inv) {
 		super.onCraftMatrixChanged(inv);
 		ItemStack output = tile.findRecipeOutput();
-		if (output != null && tile.isLast()) {
-			Utils.addItemToolTip(output, "tip", EnumChatFormatting.YELLOW + StringUtils.localize("gui.clickcraft"));
-		}
 		craftResult.setInventorySlotContents(0, output);
 	}
 
@@ -117,39 +123,4 @@ public class ContainerAutoWorkbench extends BuildCraftContainer {
 	public boolean canInteractWith(EntityPlayer entityplayer) {
 		return tile.isUseableByPlayer(entityplayer);
 	}
-//	@Override
-//	public ItemStack transferStackInSlot(EntityPlayer pl, int i) {
-//		ItemStack itemstack = null;
-//		Slot slot = (Slot) inventorySlots.get(i);
-//		if (slot != null && slot.getHasStack()) {
-//			ItemStack itemstack1 = slot.getStack();
-//			itemstack = itemstack1.copy();
-//			if (i == 0) {
-//				if (!mergeItemStack(itemstack1, 10, 46, true)) {
-//					return null;
-//				}
-//			} else if (i >= 10 && i < 37) {
-//				if (!mergeItemStack(itemstack1, 37, 46, false)) {
-//					return null;
-//				}
-//			} else if (i >= 37 && i < 46) {
-//				if (!mergeItemStack(itemstack1, 10, 37, false)) {
-//					return null;
-//				}
-//			} else if (!mergeItemStack(itemstack1, 10, 46, false)) {
-//				return null;
-//			}
-//			if (itemstack1.stackSize == 0) {
-//				slot.putStack(null);
-//			} else {
-//				slot.onSlotChanged();
-//			}
-//			if (itemstack1.stackSize != itemstack.stackSize) {
-//				slot.onPickupFromSlot(pl, itemstack1);
-//			} else {
-//				return null;
-//			}
-//		}
-//		return itemstack;
-//	}
 }
