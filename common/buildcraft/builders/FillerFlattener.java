9891f99f252c16c5e42c5ce20b0d1e6ae0ae89a8
CovertJaguar <CovertJaguar@gmail.com>
2013-06-11 21:29:08 -0700

    Make Flatten less stupid
    It now processes in columns, greatly increasing the performance.
    
    It is no longer halted entirely by anything it can't break. In other
    words, you can use it to fill over bedrock.
    
    Changed how the clear portion works, it no longer clears all the way to
    sky, instead it only clears IN the box.

--- common/buildcraft/builders/FillerFlattener.java
+++ common/buildcraft/builders/FillerFlattener.java
@@ -25,52 +25,36 @@ public class FillerFlattener extends FillerPattern {
 		int zMin = (int) box.pMin().z;
 
 		int xMax = (int) box.pMax().x;
+		int yMax = (int) box.pMax().y;
 		int zMax = (int) box.pMax().z;
 
-		int sizeX = xMax - xMin + 1;
-		int sizeZ = zMax - zMin + 1;
-
-		boolean[][] blockedColumns = new boolean[sizeX][sizeZ];
-
-		boolean found;
 		int lastX = Integer.MAX_VALUE, lastY = Integer.MAX_VALUE, lastZ = Integer.MAX_VALUE;
 
-		for (int y = yMin - 1; y >= 0; --y) {
-			found = false;
-			for (int x = xMin; x <= xMax; ++x) {
-				for (int z = zMin; z <= zMax; ++z) {
-					if (!BlockUtil.canChangeBlock(tile.worldObj, x, y, z)) {
-						return true;
-					}
-					if (!blockedColumns[x - xMin][z - zMin]) {
-						if (!BlockUtil.isSoftBlock(tile.worldObj, x, y, z)) {
-							blockedColumns[x - xMin][z - zMin] = true;
-						} else {
-							found = true;
-							lastX = x;
-							lastY = y;
-							lastZ = z;
-						}
+		boolean found = false;
+		for (int x = xMin; x <= xMax && !found; ++x) {
+			for (int z = zMin; z <= zMax && !found; ++z) {
+				for (int y = yMin - 1; y > 0; --y) {
+					if (!BlockUtil.canChangeBlock(tile.worldObj, x, y, z) || !BlockUtil.isSoftBlock(tile.worldObj, x, y, z)) {
+						break;
+					} else {
+						found = true;
+						lastX = x;
+						lastY = y;
+						lastZ = z;
 					}
 				}
 			}
-
-			if (!found) {
-				break;
-			}
-		}
-
-		if (lastX != Integer.MAX_VALUE && stackToPlace != null) {
-			BlockUtil.breakBlock(tile.worldObj, lastX, lastY - 1, lastZ);
-			stackToPlace.getItem().onItemUse(stackToPlace, CoreProxy.proxy.getBuildCraftPlayer(tile.worldObj), tile.worldObj, lastX, lastY - 1, lastZ, 1, 0.0f,
-					0.0f, 0.0f);
 		}
 
 		if (lastX != Integer.MAX_VALUE) {
+			if (stackToPlace != null) {
+				BlockUtil.breakBlock(tile.worldObj, lastX, lastY, lastZ);
+				stackToPlace.getItem().onItemUse(stackToPlace, CoreProxy.proxy.getBuildCraftPlayer(tile.worldObj), tile.worldObj, lastX, lastY - 1, lastZ, 1, 0.0f, 0.0f, 0.0f);
+			}
 			return false;
 		}
 
-		return !empty(xMin, yMin, zMin, xMax, 64 * 4, zMax, tile.worldObj);
+		return !empty(xMin, yMin, zMin, xMax, yMax, zMax, tile.worldObj);
 	}
 
 	@SideOnly(Side.CLIENT)
