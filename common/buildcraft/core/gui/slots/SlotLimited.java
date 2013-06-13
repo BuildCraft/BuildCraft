d875dc4f7ca69ec6de706dff7045041d4a0e5bc1
CovertJaguar <CovertJaguar@gmail.com>
2013-06-12 21:41:25 -0700

    Wrong header (oops)

--- common/buildcraft/core/gui/slots/SlotLimited.java
+++ common/buildcraft/core/gui/slots/SlotLimited.java
@@ -1,11 +1,3 @@
-/*
- * Copyright (c) CovertJaguar, 2011 http://railcraft.info
- * 
- * This code is the property of CovertJaguar
- * and may only be used with explicit written
- * permission unless otherwise specified on the
- * license page at railcraft.wikispaces.com.
- */
 package buildcraft.core.gui.slots;
 
 import net.minecraft.inventory.IInventory;
