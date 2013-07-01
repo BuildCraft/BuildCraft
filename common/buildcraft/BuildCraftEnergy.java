670c2684e72a166f6e67e5589886461b8b59c1a6
CovertJaguar <CovertJaguar@gmail.com>
2013-06-30 17:52:57 -0700

    Fix Iron Engine heat issues

--- common/buildcraft/BuildCraftEnergy.java
+++ common/buildcraft/BuildCraftEnergy.java
@@ -177,7 +177,7 @@ public class BuildCraftEnergy {
 		IronEngineFuel.fuels.add(new IronEngineFuel(LiquidDictionary.getLiquid("Fuel", LiquidContainerRegistry.BUCKET_VOLUME), 6, 100000));
 
 		// Iron Engine Coolants
-		IronEngineCoolant.addCoolant(new LiquidStack(Block.waterStill, LiquidContainerRegistry.BUCKET_VOLUME), 0.0025F);
+		IronEngineCoolant.addCoolant(new LiquidStack(Block.waterStill, LiquidContainerRegistry.BUCKET_VOLUME), 0.0023F);
 		IronEngineCoolant.addCoolant(Block.ice.blockID, 0, new LiquidStack(Block.waterStill, LiquidContainerRegistry.BUCKET_VOLUME * 2));
 
 		LiquidContainerRegistry.registerLiquid(new LiquidContainerData(LiquidDictionary.getLiquid("Oil", LiquidContainerRegistry.BUCKET_VOLUME), new ItemStack(
--- common/buildcraft/energy/TileEngineIron.java
+++ common/buildcraft/energy/TileEngineIron.java
@@ -43,7 +43,7 @@ import net.minecraftforge.liquids.ITankContainer;
 public class TileEngineIron extends TileEngine implements ITankContainer {
 
 	public static int MAX_LIQUID = LiquidContainerRegistry.BUCKET_VOLUME * 10;
-	public static float HEAT_PER_MJ = 0.01F;
+	public static float HEAT_PER_MJ = 0.0023F;
 	public static float COOLDOWN_RATE = 0.005F;
 	int burnTime = 0;
 	private LiquidTank fuelTank;
@@ -199,7 +199,7 @@ public class TileEngineIron extends TileEngine implements ITankContainer {
 			}
 		}
 
-		if (heat > 0 && (penaltyCooling > 0 || !isRedstonePowered)) {
+		if (heat > MIN_HEAT && (penaltyCooling > 0 || !isRedstonePowered)) {
 			heat -= COOLDOWN_RATE;
 
 		}
