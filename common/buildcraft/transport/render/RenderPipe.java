ea9331e4b26f60be560dc43ee663bc03a7077b07
Soaryn <Nick@laster.com>
2013-08-03 20:44:49 -0400

    Fixes Spacing formatting on RenderPipe Edits

--- common/buildcraft/transport/render/RenderPipe.java
+++ common/buildcraft/transport/render/RenderPipe.java
@@ -93,11 +93,11 @@ public class RenderPipe extends TileEntitySpecialRenderer {
 		}
 		block.texture = fluid.getStillIcon();
 
-                int color = fluid.getColor();
+		int color = fluid.getColor();
                 
-        float c1 = (float) (color >> 16 & 255) / 255.0F;
-        float c2 = (float) (color >> 8 & 255) / 255.0F;
-        float c3 = (float) (color & 255) / 255.0F;
+		float c1 = (float) (color >> 16 & 255) / 255.0F;
+		float c2 = (float) (color >> 8 & 255) / 255.0F;
+		float c3 = (float) (color & 255) / 255.0F;
 
 		float size = Utils.pipeMaxPos - Utils.pipeMinPos;
 
