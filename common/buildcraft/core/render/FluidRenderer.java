95fa1078607f66eb3616abd1feefeef893a787fd
Soaryn <Nick@laster.com>
2013-08-03 21:38:57 -0400

    Adds colorFluid Helper to FluidRenderer

--- common/buildcraft/core/render/FluidRenderer.java
+++ common/buildcraft/core/render/FluidRenderer.java
@@ -75,6 +75,15 @@ public class FluidRenderer {
 	public static ResourceLocation getFluidSheet(Fluid liquid) {
 		return BLOCK_TEXTURE;
 	}
+	
+	public static void colorFluid(FluidStack fluidstack){
+	    if(fluidstack== null) return;
+	    int color = fluidstack.getFluid().getColor(fluidstack);
+            float red = (float) (color >> 16 & 255) / 255.0F;
+            float green = (float) (color >> 8 & 255) / 255.0F;
+            float blue = (float) (color & 255) / 255.0F;
+            GL11.glColor4f(red, green, blue, 1);
+	}
 
 	public static int[] getFluidDisplayLists(FluidStack fluidStack, World world, boolean flowing) {
 		if (fluidStack == null) {
