871638dddaca4160dd082fe93b19a87653c2209a
SpaceToad <spacetoadcraft@gmail.com>
2014-04-09 08:54:28 +0200

    fixed missing import

--- common/buildcraft/BuildCraftEnergy.java
+++ common/buildcraft/BuildCraftEnergy.java
@@ -35,6 +35,7 @@ import buildcraft.core.DefaultProps;
 import buildcraft.core.InterModComms;
 import buildcraft.core.Version;
 import buildcraft.core.proxy.CoreProxy;
+import buildcraft.core.render.BlockHighlightHandler;
 import buildcraft.core.triggers.BCTrigger;
 import buildcraft.energy.BlockBuildcraftFluid;
 import buildcraft.energy.BlockEnergyEmitter;
