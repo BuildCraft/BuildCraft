a65f6f5bb60556baebb517a78faeed4585847949
SpaceToad <spacetoadcraft@gmail.com>
2014-04-13 18:05:32 +0200

    standardized headers

--- common/buildcraft/energy/TileEngineCreative.java
+++ common/buildcraft/energy/TileEngineCreative.java
@@ -1,14 +1,22 @@
+/**
+ * Copyright (c) 2011-2014, SpaceToad and the BuildCraft Team
+ * http://www.mod-buildcraft.com
+ *
+ * BuildCraft is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * http://www.mod-buildcraft.com/MMPL-1.0.txt
+ */
 package buildcraft.energy;
 
-import buildcraft.api.tools.IToolWrench;
-import buildcraft.core.utils.StringUtils;
-import buildcraft.transport.pipes.PipePowerIron;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.util.ForgeDirection;
+import buildcraft.api.tools.IToolWrench;
+import buildcraft.core.utils.StringUtils;
+import buildcraft.transport.pipes.PipePowerIron;
 
 public class TileEngineCreative extends TileEngine {
 
