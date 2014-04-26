7a7a350c9e876b36086efc68596fc5694f6a974f
dmillerw <dmillerw@gmail.com>
2014-04-26 14:45:59 -0700

    Finished the medium boiler models

--- common/buildcraft/factory/render/ModelTower.java
+++ common/buildcraft/factory/render/ModelTower.java
@@ -7,7 +7,9 @@ import net.minecraft.util.ResourceLocation;
 public class ModelTower {
 
 	public static final ResourceLocation TEXTURE_BASE_SMALL = new ResourceLocation("buildcraft:textures/models/tower_base_small.png");
+	public static final ResourceLocation TEXTURE_BASE_MEDIUM = new ResourceLocation("buildcraft:textures/models/tower_base_medium.png");
 	public static final ResourceLocation TEXTURE_TOP_SMALL = new ResourceLocation("buildcraft:textures/models/tower_top_small.png");
+	public static final ResourceLocation TEXTURE_TOP_MEDIUM = new ResourceLocation("buildcraft:textures/models/tower_top_medium.png");
 
 	public static class BaseSmall extends ModelBase {
 
@@ -21,6 +23,34 @@ public class ModelTower {
 			towerBase.addBox(0F, 0F, 0F, 48, 16, 48);
 			towerBase.setRotationPoint(-24F, 8F, -24F);
 			towerBase.setTextureSize(256, 64);
+			towerBase.mirror = true;
+			setRotation(towerBase, 0F, 0F, 0F);
+		}
+
+		public void render(float f5) {
+			towerBase.render(f5);
+		}
+
+		private void setRotation(ModelRenderer model, float x, float y, float z) {
+			model.rotateAngleX = x;
+			model.rotateAngleY = y;
+			model.rotateAngleZ = z;
+		}
+
+	}
+
+	public static class BaseMedium extends ModelBase {
+
+		ModelRenderer towerBase;
+
+		public BaseMedium() {
+			textureWidth = 512;
+			textureHeight = 128;
+
+			towerBase = new ModelRenderer(this, 0, 0);
+			towerBase.addBox(0F, 0F, 0F, 80, 16, 80);
+			towerBase.setRotationPoint(-40F, 8F, -40F);
+			towerBase.setTextureSize(512, 128);
 			towerBase.mirror = true;
 			setRotation(towerBase, 0F, 0F, 0F);
 		}
@@ -70,6 +100,58 @@ public class ModelTower {
 			top.addBox(0F, 0F, 0F, 48, 1, 48);
 			top.setRotationPoint(-24F, -8F, -24F);
 			top.setTextureSize(256, 128);
+			top.mirror = true;
+			setRotation(top, 0F, 0F, 0F);
+		}
+
+		public void render(float f5) {
+			tower.render(f5);
+			ring.render(f5);
+			bottom.render(f5);
+			top.render(f5);
+		}
+
+		private void setRotation(ModelRenderer model, float x, float y, float z) {
+			model.rotateAngleX = x;
+			model.rotateAngleY = y;
+			model.rotateAngleZ = z;
+		}
+
+	}
+
+	public static class TopMedium extends ModelBase {
+
+		ModelRenderer tower;
+		ModelRenderer ring;
+		ModelRenderer bottom;
+		ModelRenderer top;
+
+		public TopMedium() {
+			textureWidth = 512;
+			textureHeight = 256;
+
+			tower = new ModelRenderer(this, 0, 83);
+			tower.addBox(0F, 0F, 0F, 78, 31, 78);
+			tower.setRotationPoint(-39F, -7F, -39F);
+			tower.setTextureSize(512, 256);
+			tower.mirror = true;
+			setRotation(tower, 0F, 0F, 0F);
+			ring = new ModelRenderer(this, 0, 0);
+			ring.addBox(0F, 0F, 0F, 80, 2, 80);
+			ring.setRotationPoint(-40F, 7F, -40F);
+			ring.setTextureSize(512, 256);
+			ring.mirror = true;
+			setRotation(ring, 0F, 0F, 0F);
+			bottom = new ModelRenderer(this, 0, 0);
+			bottom.addBox(0F, 0F, 0F, 80, 1, 80);
+			bottom.setRotationPoint(-40F, 23F, -40F);
+			bottom.setTextureSize(512, 256);
+			bottom.mirror = true;
+			setRotation(bottom, 0F, 0F, 0F);
+			top = new ModelRenderer(this, 0, 0);
+			top.addBox(0F, 0F, 0F, 80, 1, 80);
+			top.setRotationPoint(-40F, -8F, -40F);
+			top.setTextureSize(512, 256);
 			top.mirror = true;
 			setRotation(top, 0F, 0F, 0F);
 		}
--- common/buildcraft/factory/render/RenderTower.java
+++ common/buildcraft/factory/render/RenderTower.java
@@ -9,11 +9,15 @@ import org.lwjgl.opengl.GL11;
 public class RenderTower extends TileEntitySpecialRenderer {
 
 	public static ModelTower.BaseSmall towerBaseSmall;
+	public static ModelTower.BaseMedium towerBaseMedium;
 	public static ModelTower.TopSmall towerTopSmall;
+	public static ModelTower.TopMedium towerTopMedium;
 
 	static {
 		towerBaseSmall = new ModelTower.BaseSmall();
+		towerBaseMedium = new ModelTower.BaseMedium();
 		towerTopSmall = new ModelTower.TopSmall();
+		towerTopMedium = new ModelTower.TopMedium();
 	}
 
 	public void renderTowerAt(TileTowerRegulator tile, double x, double y, double z, float partial) {
@@ -33,6 +37,17 @@ public class RenderTower extends TileEntitySpecialRenderer {
 					GL11.glTranslated(0, -1, 0);
 					if (i % 2 != 0) {
 						towerTopSmall.render(0.0625F);
+					}
+				}
+			} else if (tile.size == 5) {
+				Minecraft.getMinecraft().renderEngine.bindTexture(ModelTower.TEXTURE_BASE_MEDIUM);
+				towerBaseMedium.render(0.0625F);
+
+				Minecraft.getMinecraft().renderEngine.bindTexture(ModelTower.TEXTURE_TOP_MEDIUM);
+				for (int i = 1; i < tile.height - 1; i++) {
+					GL11.glTranslated(0, -1, 0);
+					if (i % 2 != 0) {
+						towerTopMedium.render(0.0625F);
 					}
 				}
 			}
