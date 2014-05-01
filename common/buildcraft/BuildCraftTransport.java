f7d466c172feb9f95b88df01a4bec9d3591746e4
SpaceToad <spacetoadcraft@gmail.com>
2014-05-01 15:35:40 +0200

    removed obsolete object default ids

--- common/buildcraft/BuildCraftTransport.java
+++ common/buildcraft/BuildCraftTransport.java
@@ -316,48 +316,44 @@ public class BuildCraftTransport extends BuildCraftMod {
 			genericPipeBlock = new BlockGenericPipe();
 			CoreProxy.proxy.registerBlock(genericPipeBlock.setBlockName("pipeBlock"), ItemBlock.class);
 
-			pipeItemsWood = buildPipe(DefaultProps.PIPE_ITEMS_WOOD_ID, PipeItemsWood.class, "Wooden Transport Pipe", CreativeTabBuildCraft.TIER_1, "plankWood", Blocks.glass, "plankWood");
-			pipeItemsEmerald = buildPipe(DefaultProps.PIPE_ITEMS_EMERALD_ID, PipeItemsEmerald.class, "Emerald Transport Pipe", CreativeTabBuildCraft.MISC, Items.emerald, Blocks.glass, Items.emerald);
-			pipeItemsCobblestone = buildPipe(DefaultProps.PIPE_ITEMS_COBBLESTONE_ID, PipeItemsCobblestone.class, "Cobblestone Transport Pipe", CreativeTabBuildCraft.TIER_1, "cobblestone", Blocks.glass, "cobblestone");
-			pipeItemsStone = buildPipe(DefaultProps.PIPE_ITEMS_STONE_ID, PipeItemsStone.class, "Stone Transport Pipe", CreativeTabBuildCraft.TIER_1, "stone", Blocks.glass, "stone");
-			pipeItemsQuartz = buildPipe(DefaultProps.PIPE_ITEMS_QUARTZ_ID, PipeItemsQuartz.class, "Quartz Transport Pipe", CreativeTabBuildCraft.TIER_1, Blocks.quartz_block, Blocks.glass, Blocks.quartz_block);
-			pipeItemsIron = buildPipe(DefaultProps.PIPE_ITEMS_IRON_ID, PipeItemsIron.class, "Iron Transport Pipe", CreativeTabBuildCraft.TIER_1, Items.iron_ingot, Blocks.glass, Items.iron_ingot);
-			pipeItemsGold = buildPipe(DefaultProps.PIPE_ITEMS_GOLD_ID, PipeItemsGold.class, "Golden Transport Pipe", CreativeTabBuildCraft.TIER_1, Items.gold_ingot, Blocks.glass, Items.gold_ingot);
-			pipeItemsDiamond = buildPipe(DefaultProps.PIPE_ITEMS_DIAMOND_ID, PipeItemsDiamond.class, "Diamond Transport Pipe", CreativeTabBuildCraft.TIER_1, Items.diamond, Blocks.glass, Items.diamond);
-			pipeItemsObsidian = buildPipe(DefaultProps.PIPE_ITEMS_OBSIDIAN_ID, PipeItemsObsidian.class, "Obsidian Transport Pipe", CreativeTabBuildCraft.TIER_1, Blocks.obsidian, Blocks.glass, Blocks.obsidian);
-			pipeItemsLapis = buildPipe(DefaultProps.PIPE_ITEMS_LAPIS_ID, PipeItemsLapis.class, "Lapis Transport Pipe", CreativeTabBuildCraft.MISC, Blocks.lapis_block, Blocks.glass, Blocks.lapis_block);
-			pipeItemsDaizuli = buildPipe(DefaultProps.PIPE_ITEMS_DAIZULI_ID, PipeItemsDaizuli.class, "Daizuli Transport Pipe", CreativeTabBuildCraft.MISC, Blocks.lapis_block, Blocks.glass, Items.diamond);
-			pipeItemsSandstone = buildPipe(DefaultProps.PIPE_ITEMS_SANDSTONE_ID, PipeItemsSandstone.class, "Sandstone Transport Pipe", CreativeTabBuildCraft.TIER_1, Blocks.sandstone, Blocks.glass, Blocks.sandstone);
-			pipeItemsVoid = buildPipe(DefaultProps.PIPE_ITEMS_VOID_ID, PipeItemsVoid.class, "Void Transport Pipe", CreativeTabBuildCraft.TIER_1, "dyeBlack", Blocks.glass, Items.redstone);
-			pipeItemsEmzuli = buildPipe(DefaultProps.PIPE_ITEMS_EMZULI_ID, PipeItemsEmzuli.class, "Emzuli Transport Pipe", CreativeTabBuildCraft.MISC, Blocks.lapis_block, Blocks.glass, Items.emerald);
-			pipeItemsStripes = buildPipe(0, PipeItemsStripes.class, "Stripes Transport Pipe", CreativeTabBuildCraft.TIER_3, Items.dye, Blocks.glass, Items.dye);
+			pipeItemsWood = buildPipe(PipeItemsWood.class, "Wooden Transport Pipe", CreativeTabBuildCraft.TIER_1, "plankWood", Blocks.glass, "plankWood");
+			pipeItemsEmerald = buildPipe(PipeItemsEmerald.class, "Emerald Transport Pipe", CreativeTabBuildCraft.MISC, Items.emerald, Blocks.glass, Items.emerald);
+			pipeItemsCobblestone = buildPipe(PipeItemsCobblestone.class, "Cobblestone Transport Pipe", CreativeTabBuildCraft.TIER_1, "cobblestone", Blocks.glass, "cobblestone");
+			pipeItemsStone = buildPipe(PipeItemsStone.class, "Stone Transport Pipe", CreativeTabBuildCraft.TIER_1, "stone", Blocks.glass, "stone");
+			pipeItemsQuartz = buildPipe(PipeItemsQuartz.class, "Quartz Transport Pipe", CreativeTabBuildCraft.TIER_1, Blocks.quartz_block, Blocks.glass, Blocks.quartz_block);
+			pipeItemsIron = buildPipe(PipeItemsIron.class, "Iron Transport Pipe", CreativeTabBuildCraft.TIER_1, Items.iron_ingot, Blocks.glass, Items.iron_ingot);
+			pipeItemsGold = buildPipe(PipeItemsGold.class, "Golden Transport Pipe", CreativeTabBuildCraft.TIER_1, Items.gold_ingot, Blocks.glass, Items.gold_ingot);
+			pipeItemsDiamond = buildPipe(PipeItemsDiamond.class, "Diamond Transport Pipe", CreativeTabBuildCraft.TIER_1, Items.diamond, Blocks.glass, Items.diamond);
+			pipeItemsObsidian = buildPipe(PipeItemsObsidian.class, "Obsidian Transport Pipe", CreativeTabBuildCraft.TIER_1, Blocks.obsidian, Blocks.glass, Blocks.obsidian);
+			pipeItemsLapis = buildPipe(PipeItemsLapis.class, "Lapis Transport Pipe", CreativeTabBuildCraft.MISC, Blocks.lapis_block, Blocks.glass, Blocks.lapis_block);
+			pipeItemsDaizuli = buildPipe(PipeItemsDaizuli.class, "Daizuli Transport Pipe", CreativeTabBuildCraft.MISC, Blocks.lapis_block, Blocks.glass, Items.diamond);
+			pipeItemsSandstone = buildPipe(PipeItemsSandstone.class, "Sandstone Transport Pipe", CreativeTabBuildCraft.TIER_1, Blocks.sandstone, Blocks.glass, Blocks.sandstone);
+			pipeItemsVoid = buildPipe(PipeItemsVoid.class, "Void Transport Pipe", CreativeTabBuildCraft.TIER_1, "dyeBlack", Blocks.glass, Items.redstone);
+			pipeItemsEmzuli = buildPipe(PipeItemsEmzuli.class, "Emzuli Transport Pipe", CreativeTabBuildCraft.MISC, Blocks.lapis_block, Blocks.glass, Items.emerald);
+			pipeItemsStripes = buildPipe(PipeItemsStripes.class, "Stripes Transport Pipe", CreativeTabBuildCraft.TIER_3, Items.dye, Blocks.glass, Items.dye);
 
-			pipeFluidsWood = buildPipe(DefaultProps.PIPE_LIQUIDS_WOOD_ID, PipeFluidsWood.class, "Wooden Waterproof Pipe", CreativeTabBuildCraft.TIER_2, pipeWaterproof, pipeItemsWood);
-			pipeFluidsCobblestone = buildPipe(DefaultProps.PIPE_LIQUIDS_COBBLESTONE_ID, PipeFluidsCobblestone.class, "Cobblestone Waterproof Pipe", CreativeTabBuildCraft.TIER_2, pipeWaterproof, pipeItemsCobblestone);
-			pipeFluidsStone = buildPipe(DefaultProps.PIPE_LIQUIDS_STONE_ID, PipeFluidsStone.class, "Stone Waterproof Pipe", CreativeTabBuildCraft.TIER_2, pipeWaterproof, pipeItemsStone);
-			pipeFluidsIron = buildPipe(DefaultProps.PIPE_LIQUIDS_IRON_ID, PipeFluidsIron.class, "Iron Waterproof Pipe", CreativeTabBuildCraft.TIER_2, pipeWaterproof, pipeItemsIron);
-			pipeFluidsGold = buildPipe(DefaultProps.PIPE_LIQUIDS_GOLD_ID, PipeFluidsGold.class, "Golden Waterproof Pipe", CreativeTabBuildCraft.TIER_2, pipeWaterproof, pipeItemsGold);
-			pipeFluidsEmerald = buildPipe(DefaultProps.PIPE_LIQUIDS_EMERALD_ID, PipeFluidsEmerald.class, "Emerald Waterproof Pipe", CreativeTabBuildCraft.MISC, pipeWaterproof, pipeItemsEmerald);
-			pipeFluidsSandstone = buildPipe(DefaultProps.PIPE_LIQUIDS_SANDSTONE_ID, PipeFluidsSandstone.class, "Sandstone Waterproof Pipe", CreativeTabBuildCraft.TIER_2, pipeWaterproof, pipeItemsSandstone);
-			pipeFluidsVoid = buildPipe(DefaultProps.PIPE_LIQUIDS_VOID_ID, PipeFluidsVoid.class, "Void Waterproof Pipe", CreativeTabBuildCraft.TIER_2, pipeWaterproof, pipeItemsVoid);
+			pipeFluidsWood = buildPipe(PipeFluidsWood.class, "Wooden Waterproof Pipe", CreativeTabBuildCraft.TIER_2, pipeWaterproof, pipeItemsWood);
+			pipeFluidsCobblestone = buildPipe(PipeFluidsCobblestone.class, "Cobblestone Waterproof Pipe", CreativeTabBuildCraft.TIER_2, pipeWaterproof, pipeItemsCobblestone);
+			pipeFluidsStone = buildPipe(PipeFluidsStone.class, "Stone Waterproof Pipe", CreativeTabBuildCraft.TIER_2, pipeWaterproof, pipeItemsStone);
+			pipeFluidsIron = buildPipe(PipeFluidsIron.class, "Iron Waterproof Pipe", CreativeTabBuildCraft.TIER_2, pipeWaterproof, pipeItemsIron);
+			pipeFluidsGold = buildPipe(PipeFluidsGold.class, "Golden Waterproof Pipe", CreativeTabBuildCraft.TIER_2, pipeWaterproof, pipeItemsGold);
+			pipeFluidsEmerald = buildPipe(PipeFluidsEmerald.class, "Emerald Waterproof Pipe", CreativeTabBuildCraft.MISC, pipeWaterproof, pipeItemsEmerald);
+			pipeFluidsSandstone = buildPipe(PipeFluidsSandstone.class, "Sandstone Waterproof Pipe", CreativeTabBuildCraft.TIER_2, pipeWaterproof, pipeItemsSandstone);
+			pipeFluidsVoid = buildPipe(PipeFluidsVoid.class, "Void Waterproof Pipe", CreativeTabBuildCraft.TIER_2, pipeWaterproof, pipeItemsVoid);
 
-			pipePowerWood = buildPipe(DefaultProps.PIPE_POWER_WOOD_ID, PipePowerWood.class, "Wooden Kinesis Pipe", CreativeTabBuildCraft.TIER_2, Items.redstone, pipeItemsWood);
-			pipePowerCobblestone = buildPipe(DefaultProps.PIPE_POWER_COBBLESTONE_ID, PipePowerCobblestone.class, "Cobblestone Kinesis Pipe", CreativeTabBuildCraft.TIER_2, Items.redstone, pipeItemsCobblestone);
-			pipePowerStone = buildPipe(DefaultProps.PIPE_POWER_STONE_ID, PipePowerStone.class, "Stone Kinesis Pipe", CreativeTabBuildCraft.TIER_2, Items.redstone, pipeItemsStone);
-			pipePowerQuartz = buildPipe(DefaultProps.PIPE_POWER_QUARTZ_ID, PipePowerQuartz.class, "Quartz Kinesis Pipe", CreativeTabBuildCraft.TIER_2, Items.redstone, pipeItemsQuartz);
-			pipePowerIron = buildPipe(DefaultProps.PIPE_POWER_IRON_ID, PipePowerIron.class, "Iron Kinesis Pipe", CreativeTabBuildCraft.TIER_2, Items.redstone, pipeItemsIron);
-			pipePowerGold = buildPipe(DefaultProps.PIPE_POWER_GOLD_ID, PipePowerGold.class, "Golden Kinesis Pipe", CreativeTabBuildCraft.TIER_2, Items.redstone, pipeItemsGold);
-			pipePowerDiamond = buildPipe(DefaultProps.PIPE_POWER_DIAMOND_ID, PipePowerDiamond.class, "Diamond Kinesis Pipe", CreativeTabBuildCraft.TIER_2, Items.redstone, pipeItemsDiamond);
+			pipePowerWood = buildPipe(PipePowerWood.class, "Wooden Kinesis Pipe", CreativeTabBuildCraft.TIER_2, Items.redstone, pipeItemsWood);
+			pipePowerCobblestone = buildPipe(PipePowerCobblestone.class, "Cobblestone Kinesis Pipe", CreativeTabBuildCraft.TIER_2, Items.redstone, pipeItemsCobblestone);
+			pipePowerStone = buildPipe(PipePowerStone.class, "Stone Kinesis Pipe", CreativeTabBuildCraft.TIER_2, Items.redstone, pipeItemsStone);
+			pipePowerQuartz = buildPipe(PipePowerQuartz.class, "Quartz Kinesis Pipe", CreativeTabBuildCraft.TIER_2, Items.redstone, pipeItemsQuartz);
+			pipePowerIron = buildPipe(PipePowerIron.class, "Iron Kinesis Pipe", CreativeTabBuildCraft.TIER_2, Items.redstone, pipeItemsIron);
+			pipePowerGold = buildPipe(PipePowerGold.class, "Golden Kinesis Pipe", CreativeTabBuildCraft.TIER_2, Items.redstone, pipeItemsGold);
+			pipePowerDiamond = buildPipe(PipePowerDiamond.class, "Diamond Kinesis Pipe", CreativeTabBuildCraft.TIER_2, Items.redstone, pipeItemsDiamond);
 
 			if (!BuildCraftCore.NEXTGEN_PREALPHA) {
-				pipePowerHeat = buildPipe(DefaultProps.PIPE_POWER_HEAT_ID, PipePowerHeat.class, "Heat Kinesis Pipe", CreativeTabBuildCraft.TIER_3, Blocks.furnace, pipeItemsDiamond);
+				pipePowerHeat = buildPipe(PipePowerHeat.class, "Heat Kinesis Pipe", CreativeTabBuildCraft.TIER_3, Blocks.furnace, pipeItemsDiamond);
 			}
 
-			pipeStructureCobblestone = buildPipe(DefaultProps.PIPE_STRUCTURE_COBBLESTONE_ID, PipeStructureCobblestone.class, "Cobblestone Structure Pipe", CreativeTabBuildCraft.TIER_1, Blocks.gravel, pipeItemsCobblestone);
-
-			// Fix the recipe
-			// pipeItemsStipes = createPipe(DefaultProps.PIPE_ITEMS_STRIPES_ID, PipeItemsStripes.class, "Stripes Transport Pipe", new ItemStack(Item.dyePowder,
-			// 1, 0), Block.glass, new ItemStack(Item.dyePowder, 1, 11));
+			pipeStructureCobblestone = buildPipe(PipeStructureCobblestone.class, "Cobblestone Structure Pipe", CreativeTabBuildCraft.TIER_1, Blocks.gravel, pipeItemsCobblestone);
 
 			pipeWire = new ItemPipeWire();
 			CoreProxy.proxy.registerItem(pipeWire);
@@ -489,7 +485,7 @@ public class BuildCraftTransport extends BuildCraftMod {
 		InterModComms.processIMC(event);
 	}
 
-	public static Item buildPipe(int defaultID, Class<? extends Pipe> clas,
+	public static Item buildPipe(Class<? extends Pipe> clas,
 			String descr, CreativeTabBuildCraft creativeTab,
 			Object... ingredients) {
 		String name = Character.toLowerCase(clas.getSimpleName().charAt(0)) + clas.getSimpleName().substring(1);
--- common/buildcraft/core/DefaultProps.java
+++ common/buildcraft/core/DefaultProps.java
@@ -15,7 +15,6 @@ public class DefaultProps {
 	public static final String DEPENDENCY_CORE = "required-after:BuildCraft|Core@" + Version.VERSION;
 	public static final String DEPENDENCY_TRANSPORT = "required-after:BuildCraft|Transport@" + Version.VERSION;
 
-	public static int WORLD_HEIGHT = 256;
 	public static final String NET_CHANNEL_NAME = "BC";
 	public static int NETWORK_UPDATE_RANGE = 128;
 	public static int PIPE_CONTENTS_RENDER_DIST = 24;
@@ -27,107 +26,6 @@ public class DefaultProps {
 	public static final String DEFAULT_LANGUAGE = "en_US";
 
 	public static String PUMP_DIMENSION_LIST = "+/*/*,+/-1/lava";
-
-	public static int WOODEN_GEAR_ID = 19100;
-	public static int STONE_GEAR_ID = 19101;
-	public static int IRON_GEAR_ID = 19102;
-	public static int GOLDEN_GEAR_ID = 19103;
-	public static int DIAMOND_GEAR_ID = 19104;
-	public static int TEMPLATE_ITEM_ID = 19105;
-	public static int WRENCH_ID = 19106;
-	public static int BUCKET_OIL_ID = 19107;
-	public static int REDSTONE_CRYSTAL_ID = 19108;
-	public static int PIPE_WATERPROOF_ID = 19109;
-	public static int BUCKET_FUEL_ID = 19110;
-	public static int GATE_ID = 19111;
-	public static int BUCKET_REDPLASMA_ID = 19112;
-	public static int PIPE_WIRE = 19113;
-	public static int REDSTONE_CHIPSET = 19117;
-	public static int BLUEPRINT_ITEM_ID = 19118;
-
-	// Moving to safer id range
-	public static int GATE_AUTARCHIC_ID = 19140;
-	public static int PIPE_FACADE_ID = 19141;
-	public static int PIPE_PLUG_ID = 19142;
-	public static int ROBOT_STATION_ID = 19143;
-
-	public static int PIPE_ITEMS_WOOD_ID = 19160;
-	public static int PIPE_ITEMS_COBBLESTONE_ID = 19161;
-	public static int PIPE_ITEMS_STONE_ID = 19162;
-	public static int PIPE_ITEMS_IRON_ID = 19163;
-	public static int PIPE_ITEMS_GOLD_ID = 19164;
-	public static int PIPE_ITEMS_DIAMOND_ID = 19165;
-	public static int PIPE_ITEMS_OBSIDIAN_ID = 19166;
-	public static int PIPE_ITEMS_EMERALD_ID = 19167;
-	public static int PIPE_ITEMS_QUARTZ_ID = 19168;
-	public static int PIPE_ITEMS_LAPIS_ID = 19169;
-	public static int PIPE_ITEMS_DAIZULI_ID = 19170;
-	public static int PIPE_ITEMS_EMZULI_ID = 19171;
-
-	public static int PIPE_LIQUIDS_WOOD_ID = 19180;
-	public static int PIPE_LIQUIDS_COBBLESTONE_ID = 19181;
-	public static int PIPE_LIQUIDS_STONE_ID = 19182;
-	public static int PIPE_LIQUIDS_IRON_ID = 19183;
-	public static int PIPE_LIQUIDS_GOLD_ID = 19184;
-	public static int PIPE_LIQUIDS_DIAMOND_ID = 19185;
-	public static int PIPE_LIQUIDS_OBSIDIAN_ID = 19186;
-	public static int PIPE_LIQUIDS_EMERALD_ID = 19187;
-	public static int PIPE_LIQUIDS_QUARTZ_ID = 19188;
-	public static int PIPE_LIQUIDS_LAPIS_ID = 19189;
-	public static int PIPE_LIQUIDS_DIAZULI_ID = 19110;
-
-	public static int PIPE_POWER_WOOD_ID = 19200;
-	public static int PIPE_POWER_COBBLESTONE_ID = 19201;
-	public static int PIPE_POWER_STONE_ID = 19202;
-	public static int PIPE_POWER_IRON_ID = 19203;
-	public static int PIPE_POWER_GOLD_ID = 19204;
-	public static int PIPE_POWER_DIAMOND_ID = 19205;
-	public static int PIPE_POWER_OBSIDIAN_ID = 19206;
-	public static int PIPE_POWER_EMERALD_ID = 19207;
-	public static int PIPE_POWER_QUARTZ_ID = 19208;
-	public static int PIPE_POWER_LAPIS_ID = 19209;
-	public static int PIPE_POWER_DIAZULI_ID = 19210;
-	public static int PIPE_POWER_HEAT_ID = 19211;
-
-	public static int PIPE_ITEMS_VOID_ID = 19220;
-	public static int PIPE_LIQUIDS_VOID_ID = 19221;
-	public static int PIPE_ITEMS_SANDSTONE_ID = 19222;
-	public static int PIPE_LIQUIDS_SANDSTONE_ID = 19223;
-	public static int PIPE_STRUCTURE_COBBLESTONE_ID = 19224;
-
-	public static int ROBOT_BASE_ITEM_ID = 19300;
-	public static int ROBOT_BUILDER_ITEM_ID = 19301;
-	public static int ROBOT_PICKER_ITEM_ID = 19302;
-
-	public static int MINING_WELL_ID = 1500;
-	public static int DRILL_ID = 1501;
-	public static int AUTO_WORKBENCH_ID = 1502;
-	public static int QUARRY_ID = 1503;
-	public static int MARKER_ID = 1504;
-	public static int FILLER_ID = 1505;
-	public static int BUILDER_ID = 1507;
-	public static int ARCHITECT_ID = 1508;
-	public static int FRAME_ID = 1509;
-	public static int ENGINE_ID = 1510;
-	public static int PUMP_ID = 1511;
-	public static int TANK_ID = 1512;
-	public static int GENERIC_PIPE_ID = 1513;
-	public static int REFINERY_ID = 1514;
-	public static int BLUEPRINT_LIBRARY_ID = 1515;
-	public static int LASER_ID = 1516;
-	public static int ASSEMBLY_TABLE_ID = 1517;
-	public static int PATH_MARKER_ID = 1518;
-	public static int HOPPER_ID = 1519;
-	public static int SPRING_ID = 1522;
-	public static int FILTERED_BUFFER_ID = 1523;
-	public static int FLOOD_GATE_ID = 1524;
-
-	public static int RECEIVER_ID = 1525;
-	public static int EMITTER_ID = 1526;
-
-	public static int OIL_ID = 1530;
-	public static int FUEL_ID = 1531;
-	public static int REDPLASMA_ID = 1532;
 
 	public static boolean CURRENT_CONTINUOUS = false;
 	public static double PIPES_DURABILITY = 0.25D;
