8a4b39828548505b8373b0aac5bf7b34f8b2e161
dmillerw <dmillerw@gmail.com>
2014-05-15 19:49:13 -0700

    Fixed checkstyle failure

--- common/buildcraft/transport/ItemFacade.java
+++ common/buildcraft/transport/ItemFacade.java
@@ -19,7 +19,6 @@ import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
