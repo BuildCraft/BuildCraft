6c30a2a5b9f4bfcdbc2307edb956264c4a93c3be
Raul Tambre <raul.tambre@gmail.com>
2014-05-26 14:47:48 +0300

    Make oil more opaque

--- common/buildcraft/BuildCraftEnergy.java
+++ common/buildcraft/BuildCraftEnergy.java
@@ -196,7 +196,7 @@ public class BuildCraftEnergy extends BuildCraftMod {
 
 		if (fluidOil.getBlock() == null) {
 			blockOil = new BlockBuildcraftFluid(fluidOil, Material.water).setFlammable(canOilBurn).setFlammability(0);
-			blockOil.setBlockName("blockOil").setLightOpacity(2);
+			blockOil.setBlockName("blockOil").setLightOpacity(1);
 			CoreProxy.proxy.registerBlock(blockOil);
 			fluidOil.setBlock(blockOil);
 		} else {
