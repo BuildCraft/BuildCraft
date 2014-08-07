9475faf2a86046bfa452d3282b37e7f3f77bf68f
SpaceToad <spacetoadcraft@gmail.com>
2014-08-07 09:33:03 +0200

    minor fixes from previous modifications

--- common/buildcraft/core/recipes/BuildCraftRecipe.java
+++ common/buildcraft/core/recipes/BuildCraftRecipe.java
@@ -8,11 +8,6 @@
  */
 package buildcraft.core.recipes;
 
-import java.lang.reflect.Field;
-import java.util.List;
-
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.Container;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -33,29 +28,26 @@ public class BuildCraftRecipe extends ShapedOreRecipe {
 
 	@Override
 	public boolean matches(InventoryCrafting inv, World world) {
-		try {
-			Field f = InventoryCrafting.class.getDeclaredField("eventHandler");
-
-			if (!f.isAccessible()) {
-				f.setAccessible(true);
-			}
-
-			Container container = (Container) f.get(inv);
-
-			f = Container.class.getDeclaredField("crafters");
-
-			if (!f.isAccessible()) {
-				f.setAccessible(true);
-			}
-
-			List crafters = (List) f.get(container);
-
-			for (Object p : crafters) {
-				EntityPlayer player = (EntityPlayer) p;
-			}
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
+		/*
+		 * This code is an experiment for forbidding crafting a recipe if the
+		 * player doesn't have a given attribute.
+		 * 
+		 * try { Field f =
+		 * InventoryCrafting.class.getDeclaredField("eventHandler");
+		 * 
+		 * if (!f.isAccessible()) { f.setAccessible(true); }
+		 * 
+		 * Container container = (Container) f.get(inv);
+		 * 
+		 * f = Container.class.getDeclaredField("crafters");
+		 * 
+		 * if (!f.isAccessible()) { f.setAccessible(true); }
+		 * 
+		 * List crafters = (List) f.get(container);
+		 * 
+		 * for (Object p : crafters) { EntityPlayer player = (EntityPlayer) p; }
+		 * } catch (Throwable e) { e.printStackTrace(); }
+		 */
 
 		return super.matches(inv, world);
 	}
