e78fc8a7782e6e799c8ff144ed0ed82d348fa78d
AEnterprise <aenterprise2@gmail.com>
2014-09-15 08:33:45 +0200

    fix game crashing when placing robot without a redstone board installed

--- common/buildcraft/transport/BlockGenericPipe.java
+++ common/buildcraft/transport/BlockGenericPipe.java
@@ -783,6 +783,8 @@ public class BlockGenericPipe extends BlockBuildCraft {
 						DockingStation station = pipe.container.getStation(rayTraceResult.sideHit);
 
 						if (!station.isTaken()) {
+							if (((ItemRobot) currentItem.getItem()).getRobotNBT(currentItem) == null )
+								return true;
 							EntityRobot robot = ((ItemRobot) currentItem.getItem())
 									.createRobot(currentItem, world);
 							robot.setUniqueRobotId(robot.getRegistry().getNextRobotId());
