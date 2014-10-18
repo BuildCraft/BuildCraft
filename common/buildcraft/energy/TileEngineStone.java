d691d72bcc0cd58352a9d169ee5172e2e41e4870
Andrew Dudney <andrew.dudney@gmail.com>
2014-10-18 14:05:14 -0600

    Fixed stone engine heat mechanics

--- common/buildcraft/energy/TileEngineStone.java
+++ common/buildcraft/energy/TileEngineStone.java
@@ -31,7 +31,7 @@ public class TileEngineStone extends TileEngineWithInventory {
 
 	static final float MAX_OUTPUT = 10;
 	static final float MIN_OUTPUT = MAX_OUTPUT / 3;
-	static final float TARGET_OUTPUT = 3.75f;
+	static final float TARGET_OUTPUT = .375f;
 	final float kp = 1f;
 	final float ki = 0.05f;
 	final double eLimit = (MAX_OUTPUT - MIN_OUTPUT) / ki;
