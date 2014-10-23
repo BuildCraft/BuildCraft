95427776d42047751c8dac6290a1928432169152
asiekierka <kontakt@asie.pl>
2014-10-23 15:47:28 +0200

    fix #2083 and redstoneInputSide bug

--- common/buildcraft/transport/BlockGenericPipe.java
+++ common/buildcraft/transport/BlockGenericPipe.java
@@ -679,12 +679,14 @@ public class BlockGenericPipe extends BlockBuildCraft {
 		if (isValid(pipe)) {
 			pipe.container.scheduleNeighborChange();
 
-			pipe.container.redstoneInput = world.isBlockIndirectlyGettingPowered(x, y, z) ? 15 : world
-					.getBlockPowerInput(x, y, z);
+			pipe.container.redstoneInput = world.getBlockPowerInput(x, y, z);
+			if (pipe.container.redstoneInput == 0) {
+				pipe.container.redstoneInput = world.isBlockIndirectlyGettingPowered(x, y, z) ? 15 : 0;
+			}
 
 			for (int i = 0; i < ForgeDirection.VALID_DIRECTIONS.length; i++) {
 				ForgeDirection d = ForgeDirection.getOrientation(i);
-				pipe.container.redstoneInputSide[i] = world.isBlockProvidingPowerTo(x + d.offsetY, y + d.offsetY, z
+				pipe.container.redstoneInputSide[i] = world.isBlockProvidingPowerTo(x + d.offsetX, y + d.offsetY, z
 						+ d.offsetZ, i);
 			}
 		}
