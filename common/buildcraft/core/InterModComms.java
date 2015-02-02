f65a28b9682d02fe8cb0a633dfbd2629889b00ea
big_Xplosion <bigxplosion44@gmail.com>
2015-02-02 16:16:21 +0100

    Added IMC messages for adding or removing coolants

--- common/buildcraft/core/InterModComms.java
+++ common/buildcraft/core/InterModComms.java
@@ -11,6 +11,8 @@ package buildcraft.core;
 import java.util.ArrayList;
 import java.util.List;
 
+import buildcraft.api.fuels.ICoolant;
+import buildcraft.energy.fuels.CoolantManager;
 import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
 import com.google.common.collect.Iterables;
@@ -25,6 +27,8 @@ import net.minecraft.world.biome.BiomeGenBase;
 import cpw.mods.fml.common.event.FMLInterModComms.IMCEvent;
 import cpw.mods.fml.common.event.FMLInterModComms.IMCMessage;
 
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 
 import buildcraft.BuildCraftTransport;
@@ -66,6 +70,10 @@ public final class InterModComms {
 				processAssemblyRecipeRemoveIMC(event, m);
 			} else if (m.key.equals("remove-refinery-recipe")) {
 				processRefineryRecipeRemoveIMC(event, m);
+			} else if (m.key.equals("add-coolant")) {
+				processCoolantAddIMC(event, m);
+			} else if (m.key.equals("remove-coolant")) {
+				processCoolantRemoveIMC(event, m);
 			} else {
 				BCLog.logger.warn(String.format("Received an IMC message with unknown key ('%s') from %s!", new Object[]{m.key, m.getSender()}));
 			}
@@ -246,5 +254,44 @@ public final class InterModComms {
 			BCLog.logger.warn(String.format("Received an invalid oil-gen-exclude request %s from mod %s", m.getStringValue(), m.getSender()));
 		}
 		BCLog.logger.info(String.format("Received a successful oil-gen-exclude request %s from mod %s", m.getStringValue(), m.getSender()));
+	}
+
+	public static void processCoolantAddIMC(IMCEvent event, IMCMessage m) {
+		boolean failed = false;
+		if (!m.isNBTMessage()){
+			failed = true;
+		} else {
+			NBTTagCompound tag = m.getNBTValue();
+			if (!tag.hasKey("coolant") || !tag.hasKey("degrees", 3)) {
+				failed = true;
+			} else {
+				Fluid coolant = FluidRegistry.getFluid(tag.getString("coolant"));
+				if (coolant != null) {
+					CoolantManager.INSTANCE.addCoolant(coolant, tag.getInteger("degrees"));
+				} else {
+					failed = true;
+				}
+			}
+		}
+		if (failed) {
+			BCLog.logger.warn("Received invalid coolant IMC message from mod %s!", m.getSender());
+		}
+	}
+
+	public static void processCoolantRemoveIMC(IMCEvent event, IMCMessage m) {
+		boolean failed = false;
+		if (m.isStringMessage()) {
+			ICoolant coolant = CoolantManager.INSTANCE.getCoolant(FluidRegistry.getFluid(m.getStringValue()));
+			if (coolant != null) {
+				CoolantManager.INSTANCE.getCoolants().remove(coolant);
+			} else {
+				failed = true;
+			}
+		} else {
+			failed = true;
+		}
+		if (failed) {
+			BCLog.logger.warn("Received invalid coolant IMC message from mod %s!", m.getSender());
+		}
 	}
 }
