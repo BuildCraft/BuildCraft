2898da9f59029c22fa2539ad7991adf912d7db6d
Hea3veN <hea3venmc@gmail.com>
2015-02-12 20:28:55 -0300

    add stripes handler for interaction with entities

--- common/buildcraft/BuildCraftTransport.java
+++ common/buildcraft/BuildCraftTransport.java
@@ -151,6 +151,7 @@ import buildcraft.transport.statements.TriggerPipeSignal;
 import buildcraft.transport.statements.TriggerRedstoneFaderInput;
 import buildcraft.transport.stripes.StripesHandlerArrow;
 import buildcraft.transport.stripes.StripesHandlerBucket;
+import buildcraft.transport.stripes.StripesHandlerEntityInteract;
 import buildcraft.transport.stripes.StripesHandlerPipes;
 import buildcraft.transport.stripes.StripesHandlerRightClick;
 import buildcraft.transport.stripes.StripesHandlerShears;
@@ -464,6 +465,7 @@ public class BuildCraftTransport extends BuildCraftMod {
 		PipeManager.registerStripesHandler(new StripesHandlerArrow());
 		PipeManager.registerStripesHandler(new StripesHandlerShears());
 		PipeManager.registerStripesHandler(new StripesHandlerPipes());
+		PipeManager.registerStripesHandler(new StripesHandlerEntityInteract());
 
 		PipeManager.registerPipePluggable(FacadePluggable.class, "facade");
 		PipeManager.registerPipePluggable(GatePluggable.class, "gate");
---
+++ common/buildcraft/transport/stripes/StripesHandlerEntityInteract.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2011-2014, SpaceToad and the BuildCraft Team
+ * http://www.mod-buildcraft.com
+ *
+ * BuildCraft is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * http://www.mod-buildcraft.com/MMPL-1.0.txt
+ */
+package buildcraft.transport.stripes;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.World;
+
+import net.minecraftforge.common.util.ForgeDirection;
+
+import buildcraft.api.transport.IStripesHandler;
+import buildcraft.api.transport.IStripesPipe;
+
+public class StripesHandlerEntityInteract implements IStripesHandler {
+
+	@Override
+	public StripesHandlerType getType() {
+		return StripesHandlerType.ITEM_USE;
+	}
+
+	@Override
+	public boolean shouldHandle(ItemStack stack) {
+		return true;
+	}
+
+	@Override
+	public boolean handle(World world, int x, int y, int z,
+			ForgeDirection direction, ItemStack stack, EntityPlayer player,
+			IStripesPipe pipe) {
+
+		AxisAlignedBB box = AxisAlignedBB.getBoundingBox(x, y, z, x + 1, y + 1,	z + 1);
+		List entities = world.getEntitiesWithinAABBExcludingEntity(null, box);
+		if (entities.size() <= 0) {
+			return false;
+		}
+
+		List<EntityLivingBase> livingEntities = new LinkedList<EntityLivingBase>();
+		for (Object entityObj : entities) {
+			if (entityObj instanceof EntityLivingBase) {
+				livingEntities.add((EntityLivingBase) entityObj);
+			}
+		}
+
+		player.setCurrentItemOrArmor(0, stack);
+
+		boolean successful = false;
+		Collections.shuffle(livingEntities);
+		while (livingEntities.size() > 0) {
+			EntityLivingBase entity = livingEntities.remove(0);
+
+			if (!player.interactWith(entity)) {
+				continue;
+			}
+			successful = true;
+			dropItemsExcept(stack, player, pipe, direction);
+		}
+		if (stack.stackSize > 0) {
+			pipe.sendItem(stack, direction.getOpposite());
+		}
+
+		return successful;
+	}
+
+	private void dropItemsExcept(ItemStack stack, EntityPlayer player, IStripesPipe pipe, ForgeDirection direction) {
+		for (int i = 0; i < player.inventory.getSizeInventory(); i++) {
+			ItemStack invStack = player.inventory.getStackInSlot(i);
+			if (invStack != null && invStack != stack) {
+				player.inventory.setInventorySlotContents(i, null);
+				pipe.sendItem(invStack, direction.getOpposite());
+			}
+		}
+	}
+
+}
