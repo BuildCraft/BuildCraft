5f3ea0fe0b4fc53aa7d0761d2ac749a43f0d57e8
Hea3veN <hea3venmc@gmail.com>
2015-02-20 08:46:32 -0300

    add stripes pipe retraction with void pipes

--- common/buildcraft/transport/stripes/StripesHandlerPipes.java
+++ common/buildcraft/transport/stripes/StripesHandlerPipes.java
@@ -8,6 +8,8 @@
  */
 package buildcraft.transport.stripes;
 
+import java.util.ArrayList;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
@@ -32,7 +34,7 @@ public class StripesHandlerPipes implements IStripesHandler {
 	public StripesHandlerType getType() {
 		return StripesHandlerType.ITEM_USE;
 	}
-	
+
 	@Override
 	public boolean shouldHandle(ItemStack stack) {
 		return stack.getItem() instanceof ItemPipe;
@@ -50,10 +52,10 @@ public class StripesHandlerPipes implements IStripesHandler {
 		if (world.getBlock(x, y, z) != Blocks.air) {
 			return false;
 		}
-		
+
 		Position p = new Position(x, y, z, direction);
 		p.moveBackwards(1.0d);
-		
+
 		TileEntity tile = world.getTileEntity((int) p.x, (int) p.y, (int) p.z);
 		if (!(tile instanceof TileGenericPipe)) {
 			return false;
@@ -62,23 +64,32 @@ public class StripesHandlerPipes implements IStripesHandler {
 		if (!(pipeTile.pipe.transport instanceof PipeTransportItems)) {
 			return false;
 		}
-		
 		// Checks done, start to actually do stuff
+
+		if (stack.getItem() != BuildCraftTransport.pipeItemsVoid) {
+			return extendPipe(world, x, y, z, direction, stack, player, pipeTile);
+		} else {
+			p.moveBackwards(1.0d);
+			return retractPipe(world, (int) p.x, (int) p.y, (int) p.z, direction, stack, player, pipeTile);
+		}
+	}
+
+	private boolean extendPipe(World world, int x, int y, int z, ForgeDirection direction, ItemStack stack, EntityPlayer player,
+			TileGenericPipe pipeTile) {
 		if (!copyPipeTo(world, pipeTile, x, y, z, player)) {
 			return false;
 		}
 
-		pipeTile.pipe.transport.container.initializeFromItemMetadata(stack.getItemDamage() - 1);
+		pipeTile.initializeFromItemMetadata(stack.getItemDamage());
 		Pipe<?> newPipe = BlockGenericPipe.createPipe(stack.getItem());
-		newPipe.setTile(pipeTile.pipe.container);
-		pipeTile.pipe.container.pipe = newPipe;
+		newPipe.setTile(pipeTile);
+		pipeTile.pipe = newPipe;
 		pipeTile.updateEntity(); // Needed so that the tile does computeConnections()
-		
+
 		stack.stackSize--;
 		if (stack.stackSize > 0) {
-			pipeTile.pipe.container.injectItem(stack, true, direction.getOpposite());
+			pipeTile.injectItem(stack, true, direction.getOpposite());
 		}
-
 		return true;
 	}
 
@@ -87,6 +98,34 @@ public class StripesHandlerPipes implements IStripesHandler {
 		ItemStack stack = new ItemStack(BuildCraftTransport.pipeItemsStripes, 1, meta);
 		if (!BuildCraftTransport.pipeItemsStripes.onItemUse(stack, player, world, x, y, z, 1, 0, 0, 0)) {
 			return false;
+		}
+		return true;
+	}
+
+	private boolean retractPipe(World world, int x, int y, int z, ForgeDirection direction, ItemStack stack, EntityPlayer player,
+			TileGenericPipe pipeTile) {
+		TileEntity prevTile = world.getTileEntity(x, y, z);
+		if (!(prevTile instanceof TileGenericPipe)) {
+			return false;
+		}
+		TileGenericPipe prevPipeTile = (TileGenericPipe) prevTile;
+		if (!(prevPipeTile.pipe.transport instanceof PipeTransportItems)) {
+			return false;
+		}
+
+		int meta = prevPipeTile.getItemMetadata();
+		ArrayList<ItemStack> removedPipeStacks = world.getBlock(x, y, z).getDrops(world, x, y, z, meta, 0);
+
+		prevPipeTile.initializeFromItemMetadata(pipeTile.getItemMetadata());
+		Pipe<?> newPipe = BlockGenericPipe.createPipe(BuildCraftTransport.pipeItemsStripes);
+		newPipe.setTile(prevPipeTile);
+		prevPipeTile.pipe = newPipe;
+
+		world.setBlockToAir(pipeTile.x(), pipeTile.y(), pipeTile.z());
+
+		prevPipeTile.injectItem(stack, true, direction);
+		for (ItemStack itemStack : removedPipeStacks) {
+			prevPipeTile.injectItem(itemStack, true, direction);
 		}
 		return true;
 	}
