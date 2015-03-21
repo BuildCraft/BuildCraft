b59ce919452c4938b141ecd99be1eb85de6b18ce
asiekierka <kontakt@asie.pl>
2015-03-21 18:21:42 +0100

    no more recursive list matching

--- common/buildcraft/core/ItemList.java
+++ common/buildcraft/core/ItemList.java
@@ -201,7 +201,7 @@ public class ItemList extends ItemBuildCraft implements IList {
 				return oreMatch(stacks[0], item);
 			} else {
 				for (ItemStack stack : stacks) {
-					if (stack != null && StackHelper.isMatchingItemOrList(stack, item)) {
+					if (stack != null && StackHelper.isMatchingItem(stacks[0], item, true, false)) {
 						return true;
 					}
 				}
