b53a1ec0a9f5329b41033f1ea9ca7a30aec3ded2
asiekierka <kontakt@asie.pl>
2015-05-05 09:54:31 +0200

    fix add-assembly-recipe IMC

--- common/buildcraft/core/InterModComms.java
+++ common/buildcraft/core/InterModComms.java
@@ -75,7 +75,7 @@ public final class InterModComms {
 		} else {
 			NBTTagCompound recipe = msg.getNBTValue();
 			if (!recipe.hasKey("id") || !recipe.hasKey("input", 9) || !recipe.hasKey("output", 10)
-					|| !recipe.hasKey("energy", 6)) { // Ints - NBTBase#NBTTypes
+					|| !recipe.hasKey("energy", 3)) { // Ints - NBTBase#NBTTypes
 				failed = true;
 			} else {
 				NBTTagList list = (NBTTagList) recipe.getTag("input");
