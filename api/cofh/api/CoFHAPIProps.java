7ee124318198d976c7347fcc563496f0353a9ac7
AlexIIL <alexj9@me.com>
2015-06-03 13:02:19 +0100

    Update CofhAPI to my version

--- api/cofh/api/CoFHAPIProps.java
+++ api/cofh/api/CoFHAPIProps.java
@@ -6,6 +6,7 @@ public class CoFHAPIProps {
 
 	}
 
-	public static final String VERSION = "1.7.10R1.0.1";
+	// Until CofH updates, this is the version number
+	public static final String VERSION = "1.8-BuildCraft-Testing";
 
 }
