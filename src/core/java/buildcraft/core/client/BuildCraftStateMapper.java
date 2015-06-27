a427b8afad952afcc8766ec9771c9764adb7ff81
AlexIIL <alexj9@me.com>
2015-06-27 21:35:13 +0100

    Remove debugging message

--- src/core/java/buildcraft/core/client/BuildCraftStateMapper.java
+++ src/core/java/buildcraft/core/client/BuildCraftStateMapper.java
@@ -6,9 +6,6 @@ import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.util.ResourceLocation;
 
-import buildcraft.api.core.BCLog;
-import buildcraft.energy.BuildCraftEnergy;
-
 public class BuildCraftStateMapper extends StateMapperBase {
     public static final BuildCraftStateMapper INSTANCE = new BuildCraftStateMapper();
 
@@ -18,9 +15,6 @@ public class BuildCraftStateMapper extends StateMapperBase {
 
     @Override
     protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
-        if (state.getBlock() == BuildCraftEnergy.blockOil) {
-            BCLog.logger.info("FFS ME! " + state);
-        }
         ResourceLocation location = (ResourceLocation) Block.blockRegistry.getNameForObject(state.getBlock());
         location = new ResourceLocation(location.getResourceDomain().replace("|", ""), location.getResourcePath());
         return new ModelResourceLocation(location, getPropertyString(state));
