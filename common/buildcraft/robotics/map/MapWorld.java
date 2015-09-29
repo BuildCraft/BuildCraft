eb2a6cb187b60587dab6cacfb182166639c5ae8d
asiekierka <kontakt@asie.pl>
2015-09-29 10:18:56 +0200

    BuildCraft 7.0.26

--- common/buildcraft/robotics/map/MapWorld.java
+++ common/buildcraft/robotics/map/MapWorld.java
@@ -1,9 +1,7 @@
 package buildcraft.robotics.map;
 
-import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
