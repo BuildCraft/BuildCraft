24e56c79ee6ac96c6bc406ade748a73ef058b66f
iTitus <iTitus@users.noreply.github.com>
2015-11-15 00:04:28 +0100

    Fix #3128 Use the Fluid's method for getting the color multiplier. I don't think that this has a serious performance impact, except when mods do heavy things in that method.

--- common/buildcraft/factory/render/RenderTank.java
+++ common/buildcraft/factory/render/RenderTank.java
@@ -30,10 +30,13 @@ public class RenderTank extends TileEntitySpecialRenderer {
 		TileTank tank = (TileTank) CoreProxy.proxy.getServerTile(tileentity);
 
 		FluidStack liquid = tank.tank.getFluid();
-		int color = tank.tank.colorRenderCache;
-		if (liquid == null || liquid.amount <= 0) {
+		if (liquid == null || liquid.getFluid() == null || liquid.amount <= 0) {
 			return;
 		}
+
+		// Workaround: The colorRenderCache from the server tile from getServerTile(...) does not get synced properly
+		// int color = tank.tank.colorRenderCache;
+		int color = liquid.getFluid().getColor(liquid);
 
 		int[] displayList = FluidRenderer.getFluidDisplayLists(liquid, tileentity.getWorldObj(), false);
 		if (displayList == null) {
