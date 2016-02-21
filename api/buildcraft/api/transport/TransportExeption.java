364b9e61042dfe9ce9e4f3b3a9fc211265022661
n00b7 <n00b7@users.noreply.github.com>
2016-02-21 12:19:42 -0500

    Create TransportExeption.java

---
+++ api/buildcraft/api/transport/TransportExeption.java
@@ -0,0 +1,11 @@
+package buildcraft.api.transport;
+
+/**
+*this is if there is a fluid transport errors
+*catch exeption if there is a problem to prevent crashing
+**/
+
+public class TransportExepton extends java.lang.Throwable
+{
+//i will want it to remove fluid/item from pipe
+}
