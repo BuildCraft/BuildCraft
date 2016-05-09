59e714686158563c298ac8c30b06f8edcd7f1a1d
AlexIIL <alexj9@me.com>
2016-05-09 16:04:25 +0100

    Fix item rendering not cleaning up old item GL data.

--- common/buildcraft/transport/TransportProxyClient.java
+++ common/buildcraft/transport/TransportProxyClient.java
@@ -4,7 +4,7 @@
  * of the license located in http://www.mod-buildcraft.com/MMPL-1.0.txt */
 package buildcraft.transport;
 
-import buildcraft.transport.client.render.*;
+import net.minecraft.client.renderer.GLAllocation;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.client.registry.ClientRegistry;
 
 import buildcraft.core.lib.client.render.FluidRenderer;
 import buildcraft.transport.client.model.*;
+import buildcraft.transport.client.render.*;
 
 public class TransportProxyClient extends TransportProxy {
 
@@ -54,5 +55,10 @@ public class TransportProxyClient extends TransportProxy {
     @Override
     public void setIconProviderFromPipe(ItemPipe item, Pipe<?> dummyPipe) {
         item.setPipesIcons(dummyPipe.getIconProvider());
+    }
+
+    @Override
+    public void clearDisplayList(int displayList) {
+        GLAllocation.deleteDisplayLists(displayList);
     }
 }
