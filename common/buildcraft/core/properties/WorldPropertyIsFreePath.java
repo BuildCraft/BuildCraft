1f8d19029ed92ec28c7ed1b77b499db03abbcf91
AlexIIL <alexj9@me.com>
2016-05-27 00:23:20 +0100

    Use "Block.isPassable" instead of "Material.blocksMovement" for robot pathfinding.

--- common/buildcraft/core/properties/WorldPropertyIsFreePath.java
+++ common/buildcraft/core/properties/WorldPropertyIsFreePath.java
@@ -1,6 +1,6 @@
 package buildcraft.core.properties;
 
-import net.minecraft.block.material.Material;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -8,8 +8,7 @@ import net.minecraft.world.IBlockAccess;
 public class WorldPropertyIsFreePath extends WorldProperty {
     @Override
     public boolean get(IBlockAccess world, IBlockState state, BlockPos pos) {
-        if (world.isAirBlock(pos)) return true;
-        Material material = state.getBlock().getMaterial();
-        return !material.blocksMovement();
+        Block block = state.getBlock();
+        return block.isPassable(world, pos);
     }
 }
