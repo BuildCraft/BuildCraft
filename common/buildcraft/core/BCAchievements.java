0cac781f71fa664130f20619dd8ef0f56bf83505
AlexIIL <alexj9@me.com>
2016-06-27 00:09:58 +0100

    Woops my "add license" finger slipped and now some of new BC code is MIT.
    Completly by accident.
    
    Like, its so accidental that I'm still typing this message and I haven't realised it yet.

--- common/buildcraft/core/BCAchievements.java
+++ common/buildcraft/core/BCAchievements.java
@@ -1,3 +1,17 @@
+/* Copyright (c) 2016 AlexIIL and the BuildCraft team
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 package buildcraft.core;
 
 import net.minecraft.block.Block;
@@ -5,23 +19,20 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.stats.Achievement;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.LoaderState;
 
-import buildcraft.api.BCBlocks;
-import buildcraft.api.BCItems;
 import buildcraft.lib.AchievementPageManager;
 
 public class BCAchievements {
-//    public static final AchievementPageManager mainPage;
-//    public static final Achievement mainPageGearWood;
-//    public static final Achievement mainPageGearStone;
-//    public static final Achievement mainPageGearIron;
-//    public static final Achievement mainPageGearGold;
-//    public static final Achievement mainPageGearDiamond;
-//    public static final Achievement mainPageWrench;
-//    public static final Achievement mainPageEngineRedstone;
+    // public static final AchievementPageManager mainPage;
+    // public static final Achievement mainPageGearWood;
+    // public static final Achievement mainPageGearStone;
+    // public static final Achievement mainPageGearIron;
+    // public static final Achievement mainPageGearGold;
+    // public static final Achievement mainPageGearDiamond;
+    // public static final Achievement mainPageWrench;
+    // public static final Achievement mainPageEngineRedstone;
     // public static final Achievement mainPageEngineStirling;
     // public static final Achievement mainPageEngineCombustion;
 
@@ -29,18 +40,25 @@ public class BCAchievements {
         if (!Loader.instance().hasReachedState(LoaderState.INITIALIZATION)) {
             throw new RuntimeException("Accessed BC Achievements too early! You can only use them from init onwards!");
         }
-//        mainPage = new AchievementPageManager("BuildCraft");
-//        mainPageGearWood = register(mainPage, "woodenGear", "woodenGearAchievement", 0, 0, BCItems.coreGearWood, null);
-//        mainPageGearStone = register(mainPage, "stoneGear", "stoneGearAchievement", 2, 0, BCItems.coreGearStone, mainPageGearWood);
-//        mainPageGearIron = register(mainPage, "ironGear", "ironGearAchievement", 4, 0, BCItems.coreGearIron, mainPageGearStone);
-//        mainPageGearGold = register(mainPage, "goldGear", "goldGearAchievement", 6, 0, BCItems.coreGearGold, mainPageGearIron);
-//        mainPageGearDiamond = register(mainPage, "diamondGear", "diamondGearAchievement", 8, 0, BCItems.coreGearDiamond, mainPageGearGold);
-//        mainPageWrench = register(mainPage, "wrench", "wrenchAchievement", 3, 2, BCItems.coreWrench, mainPageGearStone);
-//        mainPageEngineRedstone = register(mainPage, "redstoneEngine", "engineAchievement1", 1, -2, BCBlocks.coreEngineRedstone, mainPageGearWood);
+        // mainPage = new AchievementPageManager("BuildCraft");
+        // mainPageGearWood = register(mainPage, "woodenGear", "woodenGearAchievement", 0, 0, BCItems.coreGearWood,
+        // null);
+        // mainPageGearStone = register(mainPage, "stoneGear", "stoneGearAchievement", 2, 0, BCItems.coreGearStone,
+        // mainPageGearWood);
+        // mainPageGearIron = register(mainPage, "ironGear", "ironGearAchievement", 4, 0, BCItems.coreGearIron,
+        // mainPageGearStone);
+        // mainPageGearGold = register(mainPage, "goldGear", "goldGearAchievement", 6, 0, BCItems.coreGearGold,
+        // mainPageGearIron);
+        // mainPageGearDiamond = register(mainPage, "diamondGear", "diamondGearAchievement", 8, 0,
+        // BCItems.coreGearDiamond, mainPageGearGold);
+        // mainPageWrench = register(mainPage, "wrench", "wrenchAchievement", 3, 2, BCItems.coreWrench,
+        // mainPageGearStone);
+        // mainPageEngineRedstone = register(mainPage, "redstoneEngine", "engineAchievement1", 1, -2,
+        // BCBlocks.coreEngineRedstone, mainPageGearWood);
     }
 
     public static void init() {
-//        MinecraftForge.EVENT_BUS.register(mainPage);
+        // MinecraftForge.EVENT_BUS.register(mainPage);
     }
 
     private static Achievement register(AchievementPageManager page, String id, String unloc, int row, int column, Item item, Achievement parent) {
--- common/buildcraft/lib/tile/TileBCInventory_Neptune.java
+++ common/buildcraft/lib/tile/TileBCInventory_Neptune.java
@@ -1,3 +1,17 @@
+/* Copyright (c) 2016 AlexIIL and the BuildCraft team
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 package buildcraft.lib.tile;
 
 import net.minecraft.item.ItemStack;
@@ -36,7 +50,7 @@ public abstract class TileBCInventory_Neptune extends TileBC_Neptune {
     protected void onSlotChange(IItemHandlerModifiable handler, int slot, ItemStack before, ItemStack after) {
         markDirty();
     }
-    
+
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
         super.writeToNBT(nbt);
