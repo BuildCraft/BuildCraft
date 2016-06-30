32151959b9c5c5724967b5346c5ce7562b88c3f6
AlexIIL <alexj9@me.com>
2016-06-30 20:36:29 +0100

    Update headers

--- common/buildcraft/factory/container/ContainerAutoCraftItems.java
+++ common/buildcraft/factory/container/ContainerAutoCraftItems.java
@@ -1,30 +1,16 @@
 /* Copyright (c) 2016 AlexIIL and the BuildCraft team
  * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
- * Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 package buildcraft.factory.container;
+
+import net.minecraft.entity.player.EntityPlayer;
 
 import buildcraft.core.lib.gui.slots.SlotBase;
 import buildcraft.core.lib.gui.slots.SlotOutput;
 import buildcraft.core.lib.gui.slots.SlotPhantom;
-import buildcraft.core.lib.gui.slots.SlotUntouchable;
-import buildcraft.factory.TileAutoWorkbench;
-import buildcraft.factory.gui.SlotWorkbench;
-import net.minecraft.entity.player.EntityPlayer;
-
 import buildcraft.factory.tile.TileAutoWorkbenchItems;
 import buildcraft.lib.gui.ContainerBCTile;
-import net.minecraft.inventory.Slot;
 
 public class ContainerAutoCraftItems extends ContainerBCTile<TileAutoWorkbenchItems> {
     private static final int PLAYER_INV_START = 115;
@@ -43,7 +29,6 @@ public class ContainerAutoCraftItems extends ContainerBCTile<TileAutoWorkbenchIt
             }
         }
 
-//        addSlotToContainer(new SlotUntouchable(craftResult, 0, 93, 27));
         addSlotToContainer(new SlotOutput(tile.invResult, 0, 124, 35));
 
         if (!tile.getWorld().isRemote) {
