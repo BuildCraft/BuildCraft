e48096ae238e65ab026bd2c6e2a44d85500f1978
Anton Danilkin <afdw@yandex.ru>
2016-07-11 14:14:05 +0300

    Filtered buffer recipe

--- common/buildcraft/transport/BCTransportRecipes.java
+++ common/buildcraft/transport/BCTransportRecipes.java
@@ -1,5 +1,8 @@
 package buildcraft.transport;
 
+import buildcraft.core.lib.utils.Utils;
+import buildcraft.lib.recipe.RecipeBuilderShaped;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
@@ -9,6 +12,18 @@ public class BCTransportRecipes {
     public static void init() {
         if (BCTransportItems.waterproof != null) {
             GameRegistry.addShapelessRecipe(new ItemStack(BCTransportItems.waterproof), new ItemStack(Items.DYE, 1, 2));
+        }
+
+        if (Utils.isRegistered(BCTransportBlocks.filteredBuffer)) {
+            ItemStack out = new ItemStack(BCTransportBlocks.filteredBuffer);
+            RecipeBuilderShaped builder = new RecipeBuilderShaped(out);
+            builder.add("w w"); // TODO: diamond pipe in center of this line
+            builder.add("wcw");
+            builder.add("wpw");
+            builder.map('w', "plankWood");
+            builder.map('p', Blocks.PISTON);
+            builder.map('c', Blocks.CHEST);
+            GameRegistry.addRecipe(builder.build());
         }
     }
 }
