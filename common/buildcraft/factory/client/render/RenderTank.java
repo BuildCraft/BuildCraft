4755dcb327f7f4ef8918401ea584ebbfa489af5a
Anton Danilkin <afdw@yandex.ru>
2016-08-27 23:05:33 +0300

    Up face fix

--- common/buildcraft/factory/client/render/RenderTank.java
+++ common/buildcraft/factory/client/render/RenderTank.java
@@ -13,7 +13,8 @@ import net.minecraft.util.math.Vec3d;
 public class RenderTank extends CullTESR<TileTank> {
     private static final Vec3d MIN_NORMAL = new Vec3d(2 / 16D + 0.01, 0 / 16D + 0.01, 2 / 16D + 0.01);
     private static final Vec3d MIN_CONNECTED = new Vec3d(2 / 16D + 0.01, 0 / 16D, 2 / 16D + 0.01);
-    private static final Vec3d MAX = new Vec3d(14 / 16D - 0.01, 16 / 16D, 14 / 16D - 0.01);
+    private static final Vec3d MAX_NORMAL = new Vec3d(14 / 16D - 0.01, 16 / 16D - 0.01, 14 / 16D - 0.01);
+    private static final Vec3d MAX_CONNECTED = new Vec3d(14 / 16D - 0.01, 16 / 16D, 14 / 16D - 0.01);
 
     public RenderTank() {}
 
@@ -25,6 +26,6 @@ public class RenderTank extends CullTESR<TileTank> {
         sideRender[EnumFacing.UP.ordinal()] = !(tile.getWorld().getBlockState(tile.getPos().offset(EnumFacing.UP)).getBlock() instanceof BlockTank && upTile != null && upTile instanceof TileTank && !((TileTank) upTile).tank.isEmpty());
         buffer.setTranslation(x, y, z);
         // TODO: use a DeltaInt for the fluid amount
-        FluidRenderer.renderFluid(FluidSpriteType.STILL, tile.tank, sideRender[EnumFacing.DOWN.ordinal()] ? MIN_NORMAL : MIN_CONNECTED, MAX, buffer, sideRender);
+        FluidRenderer.renderFluid(FluidSpriteType.STILL, tile.tank, sideRender[EnumFacing.DOWN.ordinal()] ? MIN_NORMAL : MIN_CONNECTED, sideRender[EnumFacing.UP.ordinal()] ? MAX_NORMAL : MAX_CONNECTED, buffer, sideRender);
     }
 }
