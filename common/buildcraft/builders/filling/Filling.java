b2d14861908c551f9da8297c31839cc21bc8d320
Anton Danilkin <afdw@yandex.ru>
2017-01-07 01:04:54 +0300

    Square pattern

--- common/buildcraft/builders/filling/Filling.java
+++ common/buildcraft/builders/filling/Filling.java
@@ -1,8 +1,11 @@
 package buildcraft.builders.filling;
 
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 
+import javax.vecmath.Point2i;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public enum Filling {
@@ -57,6 +60,27 @@ public enum Filling {
         return fillingPlan;
     }
 
+    public boolean[][][] generateFillingPlanByFunctionInAxis(BlockPos size, EnumFacing.Axis axis, BiFunction<Point2i, Point2i, Boolean> function) {
+        switch (axis) {
+            case X:
+                return generateFillingPlanByFunction(size, pos -> function.apply(
+                        new Point2i(pos.getY(), pos.getZ()),
+                        new Point2i(size.getY(), size.getZ())
+                ));
+            case Y:
+                return generateFillingPlanByFunction(size, pos -> function.apply(
+                        new Point2i(pos.getX(), pos.getZ()),
+                        new Point2i(size.getX(), size.getZ())
+                ));
+            case Z:
+                return generateFillingPlanByFunction(size, pos -> function.apply(
+                        new Point2i(pos.getX(), pos.getY()),
+                        new Point2i(size.getX(), size.getY())
+                ));
+        }
+        return generateFillingPlanByFunction(size, pos -> false);
+    }
+
     public boolean[][][] getFillingPlan(BlockPos size, List<IParameter> parameters) {
         EnumParameterPattern parameterPattern = (EnumParameterPattern) parameters.get(0);
         BlockPos sizeS = size.add(-1, -1, -1);
@@ -71,6 +95,18 @@ public enum Filling {
                                     ((pos.getY() == 0 || pos.getY() == sizeS.getY()) && (pos.getZ() == 0 || pos.getZ() == sizeS.getZ())) ||
                                     ((pos.getZ() == 0 || pos.getZ() == sizeS.getZ()) && (pos.getX() == 0 || pos.getX() == sizeS.getX()))
             );
+        }
+        if (parameterPattern == EnumParameterPattern.SQUARE) {
+            EnumParameterType parameterType = (EnumParameterType) parameters.get(1);
+            if (parameterType == EnumParameterType.FILLED) {
+                return generateFillingPlanByFunction(size, pos -> true);
+            } else {
+                EnumParameterAxis parameterAxis = (EnumParameterAxis) parameters.get(2);
+                return generateFillingPlanByFunctionInAxis(size, parameterAxis.axis, (pos, sizeA) ->
+                        pos.getX() == 0 || pos.getX() == sizeA.getX() - 1 ||
+                                pos.getY() == 0 || pos.getY() == sizeA.getY() - 1
+                );
+            }
         }
         return generateFillingPlanByFunction(size, pos -> false);
     }
