e2dd59ac0a6ef867f877271509c76f14aefedc32
AlexIIL <AlexIIL@users.noreply.github.com>
2017-01-08 02:58:58 +0000

    Add sanity
    Yes, reading and writing an enum with 1 value is not a bug, but a bit useless.

--- common/buildcraft/lib/net/PacketBufferBC.java
+++ common/buildcraft/lib/net/PacketBufferBC.java
@@ -256,7 +256,7 @@ public class PacketBufferBC extends PacketBuffer {
         Enum<?>[] possible = value.getClass().getEnumConstants();
         if (possible == null) throw new IllegalArgumentException("Not an enum " + value.getClass());
         if (possible.length == 0) throw new IllegalArgumentException("Tried to write an enum value without any values! How did you do this?");
-        if (possible.length == 1) throw new IllegalArgumentException("Tried to write an enum value with only 1 possibility: this is probably a bug!");
+        if (possible.length == 1) return this;
         writeFixedBits(value.ordinal(), MathHelper.log2DeBruijn(possible.length));
         return this;
     }
@@ -265,7 +265,7 @@ public class PacketBufferBC extends PacketBuffer {
     public <E extends Enum<E>> E readEnumValue(Class<E> enumClass) {
         E[] enums = enumClass.getEnumConstants();
         if (enums.length == 0) throw new IllegalArgumentException("Tried to read an enum value without any values! How did you do this?");
-        if (enums.length == 1) throw new IllegalArgumentException("Tried to read an enum value with only 1 possibility: this is probably a bug!");
+        if (enums.length == 1) return enums[0];
         int length = MathHelper.log2DeBruijn(enums.length);
         int index = readFixedBits(length);
         return enums[index];
