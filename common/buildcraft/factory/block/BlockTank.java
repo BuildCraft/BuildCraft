9aec6a40f0687f7a9b7ee6c07702b431e9d36fa2
Anton Danilkin <afdw@yandex.ru>
2017-03-30 21:17:30 +0300

    Fix tanks

--- common/buildcraft/factory/block/BlockTank.java
+++ common/buildcraft/factory/block/BlockTank.java
@@ -89,7 +89,7 @@ public class BlockTank extends BlockBCTile_Neptune implements ICustomPipeConnect
     @Override
     public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
         if (world.isRemote) {
-            return false;
+            return true;
         }
         ItemStack heldItem = player.getHeldItem(hand);
         if (heldItem.isEmpty()) {
