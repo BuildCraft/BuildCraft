16610ad7e5d84e36250e61da428aec3933f65ea7
AlexIIL <alexj9@me.com>
2017-04-20 18:16:18 +0100

    Add recipe sorting, partial fix for #3527

--- common/buildcraft/lib/BCLibRecipes.java
+++ common/buildcraft/lib/BCLibRecipes.java
@@ -7,12 +7,18 @@ import java.util.List;
 import net.minecraft.init.Items;
 
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.RecipeSorter;
+import net.minecraftforge.oredict.RecipeSorter.Category;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 import buildcraft.api.BCItems;
 
+import buildcraft.lib.recipe.NBTAwareShapedOreRecipe;
+
 public class BCLibRecipes {
     public static void fmlInit() {
+        RecipeSorter.register("buildcraftlib:nbt_aware_shaped_ore", NBTAwareShapedOreRecipe.class, Category.SHAPED, "after:forge:shapedore");
+
         if (BCItems.LIB_GUIDE != null) {
             List<Object> input = new ArrayList<>(4);
             if (BCItems.CORE_GEAR_WOOD != null) {
