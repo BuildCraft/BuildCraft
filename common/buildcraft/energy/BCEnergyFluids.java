d30edef18417a9f0631ba078470c441192dbe425
Anton Danilkin <afdw@yandex.ru>
2017-05-15 15:52:31 +0300

    Inline colors array

--- common/buildcraft/energy/BCEnergyFluids.java
+++ common/buildcraft/energy/BCEnergyFluids.java
@@ -14,15 +14,25 @@ import buildcraft.lib.fluid.FluidManager;
 
 public class BCEnergyFluids {
     public static BCFluid[] crudeOil;
-    /** All 3 fuels (no residue) */
+    /**
+     * All 3 fuels (no residue)
+     */
     public static BCFluid[] oilDistilled;
-    /** The 3 heaviest components (fuelLight, fuelDense and oilResidue) */
+    /**
+     * The 3 heaviest components (fuelLight, fuelDense and oilResidue)
+     */
     public static BCFluid[] oilHeavy;
-    /** The 2 lightest fuels (no dense fuel) */
+    /**
+     * The 2 lightest fuels (no dense fuel)
+     */
     public static BCFluid[] fuelMixedLight;
-    /** The 2 heaviest fuels (no gaseous fuel) */
+    /**
+     * The 2 heaviest fuels (no gaseous fuel)
+     */
     public static BCFluid[] fuelMixedHeavy;
-    /** The 2 heaviest products (fuelDense and oilResidue) */
+    /**
+     * The 2 heaviest products (fuelDense and oilResidue)
+     */
     public static BCFluid[] oilDense;
 
     // End products in order from least to most dense
@@ -36,56 +46,40 @@ public class BCEnergyFluids {
     public static final List<BCFluid> allFluids = new ArrayList<>();
 
     public static void preInit() {
-        int[][] colours = {//
-            { 0x50_50_50, 0x05_05_05 }, // Crude Oil
-            { 0x10_0F_10, 0x21_0F_42 },// Residue
-            { 0xA0_8F_1F, 0x42_35_20 },// Heavy Oil
-            { 0x87_6E_77, 0x42_24_24 },// Dense Oil
-            { 0xE4_BF_78, 0xA4_8F_00 },// Distilled Oil
-            { 0xFF_AF_3F, 0xE0_7F_00 },// Dense Fuel
-            { 0xF2_A7_00, 0xC4_87_00 },// Mixed Dense + Light Fuel
-            { 0xFF_FF_30, 0xE4_CF_00 },// Light Fuel
-            { 0xF6_D7_00, 0xC4_B7_00 },// Mixed Light + Gas Fuel
-            { 0xFA_F6_30, 0xE0_D9_00 },// Gas Fuel
-            { 0x3F_3F_3F, 0x30_30_30 },// Tar
-        };
         if (BCModules.FACTORY.isLoaded()) {
-            int index = 0;
-
-            // Add all of the fluid states
-            crudeOil = defineFluids("oil", 4000, 4000, 3, 6, colours[index++]);
-            oilResidue = defineFluids("oil_residue", 6000, 8000, 3, 4, colours[index++]);
-            oilHeavy = defineFluids("oil_heavy", 4000, 4000, 2, 6, colours[index++]);
-            oilDense = defineFluids("oil_dense", 5000, 5000, 3, 5, colours[index++]);
-            oilDistilled = defineFluids("oil_distilled", 3000, 3500, 2, 8, colours[index++]);
-            fuelDense = defineFluids("fuel_dense", 2000, 5000, 2, 7, colours[index++]);
-            fuelMixedHeavy = defineFluids("fuel_mixed_heavy", 1200, 700, 2, 7, colours[index++]);
-            fuelLight = defineFluids("fuel_light", 1000, 900, 1, 8, colours[index++]);
-            fuelMixedLight = defineFluids("fuel_mixed_light", 800, 700, 1, 9, colours[index++]);
-            fuelGaseous = defineFluids("fuel_gaseous", 300, 600, 0, 10, colours[index++]);
+            crudeOil = defineFluids("oil", 4000, 4000, 3, 6, 0x50_50_50, 0x05_05_05);
+            oilResidue = defineFluids("oil_residue", 6000, 8000, 3, 4, 0x10_0F_10, 0x21_0F_42);
+            oilHeavy = defineFluids("oil_heavy", 4000, 4000, 2, 6, 0xA0_8F_1F, 0x42_35_20);
+            oilDense = defineFluids("oil_dense", 5000, 5000, 3, 5, 0x87_6E_77, 0x42_24_24);
+            oilDistilled = defineFluids("oil_distilled", 3000, 3500, 2, 8, 0xE4_BF_78, 0xA4_8F_00);
+            fuelDense = defineFluids("fuel_dense", 2000, 5000, 2, 7, 0xFF_AF_3F, 0xE0_7F_00);
+            fuelMixedHeavy = defineFluids("fuel_mixed_heavy", 1200, 700, 2, 7, 0xF2_A7_00, 0xC4_87_00);
+            fuelLight = defineFluids("fuel_light", 1000, 900, 1, 8, 0xFF_FF_30, 0xE4_CF_00);
+            fuelMixedLight = defineFluids("fuel_mixed_light", 800, 700, 1, 9, 0xF6_D7_00, 0xC4_B7_00);
+            fuelGaseous = defineFluids("fuel_gaseous", 300, 600, 0, 10, 0xFA_F6_30, 0xE0_D9_00);
         } else {
-            crudeOil = new BCFluid[] { defineFluid("oil", 4000, 4000, 0, 3, 6, colours[0]) };
+            crudeOil = new BCFluid[] {defineFluid("oil", 4000, 4000, 0, 3, 6, 0x50_50_50, 0x05_05_05)};
             oilResidue = new BCFluid[0];
             oilHeavy = new BCFluid[0];
             oilDense = new BCFluid[0];
             oilDistilled = new BCFluid[0];
             fuelDense = new BCFluid[0];
             fuelMixedHeavy = new BCFluid[0];
-            fuelLight = new BCFluid[] { defineFluid("fuel_light", 1000, 900, 0, 1, 8, colours[7]) };
+            fuelLight = new BCFluid[] {defineFluid("fuel_light", 1000, 900, 0, 1, 8, 0xFF_FF_30, 0xE4_CF_00)};
             fuelMixedLight = new BCFluid[0];
             fuelGaseous = new BCFluid[0];
         }
     }
 
-    private static BCFluid[] defineFluids(String name, int density, int baseViscocity, int boilPoint, int baseQuanta, int[] texColours) {
+    private static BCFluid[] defineFluids(String name, int density, int baseViscocity, int boilPoint, int baseQuanta, int lightColor, int darkColor) {
         BCFluid[] arr = new BCFluid[3];
         for (int h = 0; h < 3; h++) {
-            arr[h] = defineFluid(name, density, baseViscocity, h, boilPoint, baseQuanta, texColours);
+            arr[h] = defineFluid(name, density, baseViscocity, h, boilPoint, baseQuanta, lightColor, darkColor);
         }
         return arr;
     }
 
-    private static BCFluid defineFluid(String name, int density, int baseViscocity, int heat, int boilPoint, int baseQuanta, int[] texColours) {
+    private static BCFluid defineFluid(String name, int density, int baseViscocity, int heat, int boilPoint, int baseQuanta, int lightColor, int darkColor) {
         String fullName = name + (heat == 0 ? "" : "_heat_" + heat);
         int tempAdjustedViscocity = baseViscocity * (4 - heat) / 4;
         int boilAdjustedDensity = density * (heat >= boilPoint ? -1 : 1);
@@ -100,16 +94,15 @@ public class BCEnergyFluids {
         def.setViscosity(tempAdjustedViscocity);
         def.setDensity(boilAdjustedDensity);
         def.setGaseous(def.getDensity() < 0);
-        def.setColour(texColours[0], texColours[1]);
-        int averageColor = texColours[1];
+        def.setColour(lightColor, darkColor);
         def.setMapColour(
                 Arrays.stream(MapColor.COLORS)
                         .filter(Objects::nonNull)
                         .filter(mapColor -> mapColor.colorValue != 0)
                         .min(Comparator.comparingInt(mapColor ->
-                                        IntMath.pow((mapColor.colorValue >> 16 & 0xFF) - (averageColor >> 16 & 0xFF), 2) +
-                                        IntMath.pow((mapColor.colorValue >> 8 & 0xFF) - (averageColor >> 8 & 0xFF), 2) +
-                                        IntMath.pow((mapColor.colorValue & 0xFF) - (averageColor & 0xFF), 2)
+                                IntMath.pow((mapColor.colorValue >> 16 & 0xFF) - (darkColor >> 16 & 0xFF), 2) +
+                                        IntMath.pow((mapColor.colorValue >> 8 & 0xFF) - (darkColor >> 8 & 0xFF), 2) +
+                                        IntMath.pow((mapColor.colorValue & 0xFF) - (darkColor & 0xFF), 2)
                         ))
                         .orElseThrow(IllegalArgumentException::new)
         );
