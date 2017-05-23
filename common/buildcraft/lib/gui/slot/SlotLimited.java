e40e7f42b5a1da6277753bbe4c32a69701ca39c0
AEnterprise <aenterprise2@gmail.com>
2017-05-23 11:25:30 +0200

    replace outdated copyright

--- common/buildcraft/lib/gui/slot/SlotLimited.java
+++ common/buildcraft/lib/gui/slot/SlotLimited.java
@@ -1,7 +1,8 @@
-/** Copyright (c) 2011-2015, SpaceToad and the BuildCraft Team http://www.mod-buildcraft.com
- * <p/>
- * BuildCraft is distributed under the terms of the Minecraft Mod Public License 1.0, or MMPL. Please check the contents
- * of the license located in http://www.mod-buildcraft.com/MMPL-1.0.txt */
+/*
+ * Copyright (c) 2017 SpaceToad and the BuildCraft team
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+ * distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/
+ */
 package buildcraft.lib.gui.slot;
 
 import buildcraft.lib.tile.item.IItemHandlerAdv;
