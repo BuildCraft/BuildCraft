e4d23b7e67a59b61715c783b0e1df8ecfd6e7338
AlexIIL <alexj9@me.com>
2017-05-30 20:40:56 +0100

    Re-enable the builders blocks.

--- common/buildcraft/builders/BCBuildersBlocks.java
+++ common/buildcraft/builders/BCBuildersBlocks.java
@@ -4,13 +4,22 @@
  * distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 package buildcraft.builders;
 
-import buildcraft.builders.block.*;
-import buildcraft.builders.tile.*;
+import net.minecraft.block.material.Material;
 
-import buildcraft.lib.BCLib;
 import buildcraft.lib.block.BlockBCBase_Neptune;
 import buildcraft.lib.tile.TileBC_Neptune;
-import net.minecraft.block.material.Material;
+
+import buildcraft.builders.block.BlockArchitectTable;
+import buildcraft.builders.block.BlockBuilder;
+import buildcraft.builders.block.BlockElectronicLibrary;
+import buildcraft.builders.block.BlockFiller;
+import buildcraft.builders.block.BlockFrame;
+import buildcraft.builders.block.BlockQuarry;
+import buildcraft.builders.tile.TileArchitectTable;
+import buildcraft.builders.tile.TileBuilder;
+import buildcraft.builders.tile.TileElectronicLibrary;
+import buildcraft.builders.tile.TileFiller;
+import buildcraft.builders.tile.TileQuarry;
 
 public class BCBuildersBlocks {
     public static BlockArchitectTable architect;
@@ -22,21 +31,17 @@ public class BCBuildersBlocks {
     public static BlockQuarry quarry;
 
     public static void preInit() {
-        if (BCLib.DEV) {
-            architect = BlockBCBase_Neptune.register(new BlockArchitectTable(Material.IRON, "block.architect"));
-            builder = BlockBCBase_Neptune.register(new BlockBuilder(Material.IRON, "block.builder"));
-            filler = BlockBCBase_Neptune.register(new BlockFiller(Material.IRON, "block.filler"));
-            library = BlockBCBase_Neptune.register(new BlockElectronicLibrary(Material.IRON, "block.library"));
-        }
+        architect = BlockBCBase_Neptune.register(new BlockArchitectTable(Material.IRON, "block.architect"));
+        builder = BlockBCBase_Neptune.register(new BlockBuilder(Material.IRON, "block.builder"));
+        library = BlockBCBase_Neptune.register(new BlockElectronicLibrary(Material.IRON, "block.library"));
+        filler = BlockBCBase_Neptune.register(new BlockFiller(Material.IRON, "block.filler"));
         frame = BlockBCBase_Neptune.register(new BlockFrame(Material.ROCK, "block.frame"));
         quarry = BlockBCBase_Neptune.register(new BlockQuarry(Material.ROCK, "block.quarry"));
 
-        if (BCLib.DEV) {
-            TileBC_Neptune.registerTile(TileArchitectTable.class, "tile.architect");
-            TileBC_Neptune.registerTile(TileBuilder.class, "tile.builder");
-            TileBC_Neptune.registerTile(TileFiller.class, "tile.filler");
-            TileBC_Neptune.registerTile(TileElectronicLibrary.class, "tile.library");
-        }
+        TileBC_Neptune.registerTile(TileArchitectTable.class, "tile.architect");
+        TileBC_Neptune.registerTile(TileBuilder.class, "tile.builder");
+        TileBC_Neptune.registerTile(TileFiller.class, "tile.filler");
+        TileBC_Neptune.registerTile(TileElectronicLibrary.class, "tile.library");
         TileBC_Neptune.registerTile(TileQuarry.class, "tile.quarry");
     }
 }
