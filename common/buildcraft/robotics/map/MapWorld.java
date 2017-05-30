50147c04a40ed17d601f021f73db4805f5f49947
AlexIIL <alexj9@me.com>
2017-05-30 20:21:14 +0100

    Fix concurrency issue related to MapWorld and saving (#3611)

--- common/buildcraft/robotics/map/MapWorld.java
+++ common/buildcraft/robotics/map/MapWorld.java
@@ -140,7 +140,9 @@ public class MapWorld {
 		long id = MapUtils.getIDFromCoords(rchunk.xPosition, rchunk.zPosition);
 		MapChunk chunk = getChunk(rchunk.xPosition, rchunk.zPosition);
 		chunk.update(rchunk);
-		updatedChunks.add(id);
+		synchronized (updatedChunks) {
+	        updatedChunks.add(id);
+        }
 		synchronized (timeToUpdate) {
 			timeToUpdate.remove(rchunk);
 		}
