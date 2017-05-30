5dc887319d66d3e1418f088d3c450c38eecca4c8
AlexIIL <alexj9@me.com>
2017-05-30 23:27:05 +0100

    Fix Box.doesInterset being wrong.

--- common/buildcraft/lib/misc/data/Box.java
+++ common/buildcraft/lib/misc/data/Box.java
@@ -250,9 +250,9 @@ public class Box implements IBox {
 
     public boolean doesIntersectWith(Box box) {
         if (isInitialized() && box.isInitialized()) {
-            return min.getX() < box.max.getX() && max.getX() > box.min.getX()//
-                && min.getY() < box.max.getY() && max.getY() > box.min.getY() //
-                && min.getZ() < box.max.getZ() && max.getZ() > box.min.getZ();
+            return min.getX() <= box.max.getX() && max.getX() >= box.min.getX()//
+                && min.getY() <= box.max.getY() && max.getY() >= box.min.getY()//
+                && min.getZ() <= box.max.getZ() && max.getZ() >= box.min.getZ();
         }
         return false;
     }
