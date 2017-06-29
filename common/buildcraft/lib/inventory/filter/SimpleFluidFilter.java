69c17341be608e4963589db79792632985222e06
Anton Danilkin <afdw@yandex.ru>
2017-06-29 15:38:11 +0300

    Add blank line after copyright

--- common/buildcraft/lib/inventory/filter/SimpleFluidFilter.java
+++ common/buildcraft/lib/inventory/filter/SimpleFluidFilter.java
@@ -3,6 +3,7 @@
  * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
  * distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/
  */
+
 package buildcraft.lib.inventory.filter;
 
 import net.minecraftforge.fluids.FluidStack;
