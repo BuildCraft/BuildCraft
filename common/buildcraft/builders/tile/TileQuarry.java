d6db9b75f27de376e2d0ba9a414559219e823009
Anton Danilkin <afdw@yandex.ru>
2017-07-02 16:49:45 +0300

    Add adapter

--- common/buildcraft/builders/tile/TileQuarry.java
+++ common/buildcraft/builders/tile/TileQuarry.java
@@ -21,7 +21,6 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 import com.google.common.collect.ImmutableList;
 
@@ -36,8 +35,6 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumFacing.Axis;
 import net.minecraft.util.ITickable;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
@@ -80,6 +77,7 @@ import buildcraft.lib.misc.data.EnumAxisOrder;
 import buildcraft.lib.mj.MjBatteryReciver;
 import buildcraft.lib.net.PacketBufferBC;
 import buildcraft.lib.tile.TileBC_Neptune;
+import buildcraft.lib.world.WorldEventListenerAdapter;
 
 import buildcraft.builders.BCBuildersBlocks;
 import buildcraft.builders.BCBuildersEventDist;
@@ -105,7 +103,7 @@ public class TileQuarry extends TileBC_Neptune implements ITickable, IDebuggable
      * Recent power input, in MJ (not micro)
      */
     private final AverageInt recentPowerAverage = new AverageInt(200);
-    private final IWorldEventListener worldEventListener = new IWorldEventListener() {
+    private final IWorldEventListener worldEventListener = new WorldEventListenerAdapter() {
         @Override
         public void notifyBlockUpdate(@Nonnull World world,
                                       @Nonnull BlockPos pos,
@@ -130,74 +128,6 @@ public class TileQuarry extends TileBC_Neptune implements ITickable, IDebuggable
                     }
                 }
             }
-        }
-
-        @Override
-        public void notifyLightSet(@Nonnull BlockPos pos) {
-        }
-
-        @Override
-        public void markBlockRangeForRenderUpdate(int x1, int y1, int z1, int x2, int y2, int z2) {
-        }
-
-        @Override
-        public void playSoundToAllNearExcept(@Nullable EntityPlayer player,
-                                             @Nonnull SoundEvent sound,
-                                             @Nonnull SoundCategory category,
-                                             double x,
-                                             double y,
-                                             double z,
-                                             float volume,
-                                             float pitch) {
-        }
-
-        @Override
-        public void playRecord(@Nonnull SoundEvent sound, @Nonnull BlockPos pos) {
-        }
-
-        @Override
-        public void spawnParticle(int particleID,
-                                  boolean ignoreRange,
-                                  double xCoord,
-                                  double yCoord,
-                                  double zCoord,
-                                  double xSpeed,
-                                  double ySpeed,
-                                  double zSpeed,
-                                  @Nonnull int... parameters) {
-        }
-
-        @Override
-        public void spawnParticle(int id,
-                                  boolean ignoreRange,
-                                  boolean minParticles,
-                                  double x,
-                                  double y,
-                                  double z,
-                                  double xSpeed,
-                                  double ySpeed,
-                                  double zSpeed,
-                                  @Nonnull int... parameters) {
-        }
-
-        @Override
-        public void onEntityAdded(@Nonnull Entity entity) {
-        }
-
-        @Override
-        public void onEntityRemoved(@Nonnull Entity entity) {
-        }
-
-        @Override
-        public void broadcastSound(int soundID, @Nonnull BlockPos pos, int data) {
-        }
-
-        @Override
-        public void playEvent(@Nullable EntityPlayer player, int type, @Nonnull BlockPos blockPos, int data) {
-        }
-
-        @Override
-        public void sendBlockBreakProgress(int breakerId, @Nonnull BlockPos pos, int progress) {
         }
     };
 
