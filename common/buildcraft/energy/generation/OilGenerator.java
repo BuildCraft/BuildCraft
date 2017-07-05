bc7e0ea6e0a287e311934d175fb49174e0331c31
AlexIIL <alexj9@me.com>
2017-07-05 22:59:35 +0100

    Fix checkstyle violation

--- common/buildcraft/energy/generation/OilGenerator.java
+++ common/buildcraft/energy/generation/OilGenerator.java
@@ -29,7 +29,7 @@ public enum OilGenerator implements IWorldGenerator {
     INSTANCE;
 
     /** Random number, used to differentiate generators */
-    private static final long MAGIC_GEN_NUMBER = 0xD0_46_B4_E4_0C_7D_07_CFl;
+    private static final long MAGIC_GEN_NUMBER = 0xD0_46_B4_E4_0C_7D_07_CFL;
 
     /** The distance that oil generation will be checked to see if their structures overlap with the currently
      * generating chunk. This should be large enough that all oil generation can fit inside this radius. If this number
