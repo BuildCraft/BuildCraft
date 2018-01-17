2b7659133226639040f49f6806912ba4345bbfc8
AlexIIL <alexj9@me.com>
2018-01-17 18:30:00 +0000

    Fix the oredictionary handler being too accepting, by moving some of its functionality into ListHandler.
     - This makes bee handlers much simpler, as they won't be force-overriden by the oredictionary one.
     - Also fix the fluid examples list not being returned.

--- common/buildcraft/lib/list/ListMatchHandlerFluid.java
+++ common/buildcraft/lib/list/ListMatchHandlerFluid.java
@@ -115,6 +115,7 @@ public class ListMatchHandlerFluid extends ListMatchHandler {
                         }
                     }
                 }
+                return examples;
             }
         }
         return null;
