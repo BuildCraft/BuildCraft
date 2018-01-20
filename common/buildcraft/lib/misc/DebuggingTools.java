1b6c7ab4dcb77c7aa182361f4cb426a311b0da4b
Anton Danilkin <afdw@yandex.ru>
2018-01-20 18:27:13 +0300

    Use WorldEventListenerAdapter

--- common/buildcraft/lib/misc/DebuggingTools.java
+++ common/buildcraft/lib/misc/DebuggingTools.java
@@ -6,21 +6,14 @@
 
 package buildcraft.lib.misc;
 
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IWorldEventListener;
-import net.minecraft.world.World;
-
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import buildcraft.api.core.BCDebugging;
 import buildcraft.api.core.BCLog;
+
+import buildcraft.lib.world.WorldEventListenerAdapter;
 
 public class DebuggingTools {
     public static final boolean ENABLE = BCDebugging.shouldDebugComplex("lib.debug.world");
@@ -38,9 +31,7 @@ public class DebuggingTools {
         }
     }
 
-    private static class WorldListener implements IWorldEventListener {
-        public WorldListener() {}
-
+    private static class WorldListener extends WorldEventListenerAdapter {
         @Override
         public void markBlockRangeForRenderUpdate(int x1, int y1, int z1, int x2, int y2, int z2) {
             StackTraceElement[] elements = new Throwable().getStackTrace();
@@ -58,38 +49,5 @@ public class DebuggingTools {
                 }
             }
         }
-
-        @Override
-        public void notifyBlockUpdate(World worldIn, BlockPos pos, IBlockState oldState, IBlockState newState, int flags) {}
-
-        @Override
-        public void notifyLightSet(BlockPos pos) {}
-
-        @Override
-        public void playSoundToAllNearExcept(EntityPlayer player, SoundEvent soundIn, SoundCategory category, double x, double y, double z, float volume, float pitch) {}
-
-        @Override
-        public void playRecord(SoundEvent soundIn, BlockPos pos) {}
-
-        @Override
-        public void spawnParticle(int particleID, boolean ignoreRange, double xCoord, double yCoord, double zCoord, double xSpeed, double ySpeed, double zSpeed, int... parameters) {}
-
-        @Override
-        public void onEntityAdded(Entity entityIn) {}
-
-        @Override
-        public void onEntityRemoved(Entity entityIn) {}
-
-        @Override
-        public void broadcastSound(int soundID, BlockPos pos, int data) {}
-
-        @Override
-        public void playEvent(EntityPlayer player, int type, BlockPos blockPosIn, int data) {}
-
-        @Override
-        public void sendBlockBreakProgress(int breakerId, BlockPos pos, int progress) {}
-
-        @Override
-        public void spawnParticle(int p_190570_1_, boolean p_190570_2_, boolean p_190570_3_, double p_190570_4_, double p_190570_6_, double p_190570_8_, double p_190570_10_, double p_190570_12_, double p_190570_14_, int... p_190570_16_) {}
     }
 }
