b7368de8f1e7ca8950a817581b48db78ec122dd6
Tencao <Chris9282@hotmail.co.uk>
2018-02-24 15:52:35 +0000

    Fix #3845

--- common/buildcraft/transport/pipe/flow/PipeFlowPower.java
+++ common/buildcraft/transport/pipe/flow/PipeFlowPower.java
@@ -6,24 +6,6 @@
 
 package buildcraft.transport.pipe.flow;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.EnumMap;
-import java.util.List;
-import java.util.function.ToLongFunction;
-
-import javax.annotation.Nonnull;
-
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.PacketBuffer;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.RayTraceResult;
-
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.fml.relauncher.Side;
-
 import buildcraft.api.core.EnumPipePart;
 import buildcraft.api.core.SafeTimeTracker;
 import buildcraft.api.mj.IMjConnector;
@@ -31,19 +13,29 @@ import buildcraft.api.mj.IMjPassiveProvider;
 import buildcraft.api.mj.IMjReceiver;
 import buildcraft.api.mj.MjAPI;
 import buildcraft.api.tiles.IDebuggable;
-import buildcraft.api.transport.pipe.IFlowPower;
-import buildcraft.api.transport.pipe.IPipe;
+import buildcraft.api.transport.pipe.*;
 import buildcraft.api.transport.pipe.IPipe.ConnectedType;
-import buildcraft.api.transport.pipe.PipeApi;
 import buildcraft.api.transport.pipe.PipeApi.PowerTransferInfo;
-import buildcraft.api.transport.pipe.PipeEventPower;
-import buildcraft.api.transport.pipe.PipeFlow;
-
+import buildcraft.core.BCCoreConfig;
 import buildcraft.lib.misc.LocaleUtil;
 import buildcraft.lib.misc.MathUtil;
 import buildcraft.lib.misc.data.AverageInt;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.fml.relauncher.Side;
 
-import buildcraft.core.BCCoreConfig;
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.function.ToLongFunction;
 
 public class PipeFlowPower extends PipeFlow implements IFlowPower, IDebuggable {
     private static final long DEFAULT_MAX_POWER = MjAPI.MJ * 10;
@@ -242,7 +234,13 @@ public class PipeFlowPower extends PipeFlow implements IFlowPower, IDebuggable {
                         }
                         Section s2 = sections.get(face2);
                         if (s2.powerQuery > 0) {
-                            long watts = Math.min(s.internalPower * s2.powerQuery / unusedPowerQuery, s.internalPower);
+                            long watts = Math.min(
+                                    BigInteger.valueOf(s.internalPower)
+                                            .multiply(BigInteger.valueOf(s2.powerQuery))
+                                            .divide(BigInteger.valueOf(unusedPowerQuery))
+                                            .longValue(),
+                                    s.internalPower
+                            );
                             unusedPowerQuery -= s2.powerQuery;
                             IPipe neighbour = pipe.getConnectedPipe(face2);
                             long leftover = watts;
