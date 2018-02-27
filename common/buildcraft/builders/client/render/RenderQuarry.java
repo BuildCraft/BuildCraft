a30093c53c96fb73eb88e0dd8272f93293ed129e
Anton Danilkin <afdw@yandex.ru>
2018-02-27 10:45:35 +0300

    Fix #3838

--- common/buildcraft/builders/client/render/RenderQuarry.java
+++ common/buildcraft/builders/client/render/RenderQuarry.java
@@ -17,6 +17,7 @@ import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.profiler.Profiler;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
@@ -100,7 +101,7 @@ public class RenderQuarry extends TileEntitySpecialRenderer<TileQuarry> {
 
         profiler.endSection();
         if (tile.frameBox.isInitialized()) {
-            double yOffset = 1;
+            double yOffset = 1 + 4 / 16D;
 
             profiler.startSection("laser");
             if (tile.currentTask != null && tile.currentTask instanceof TileQuarry.TaskBreakBlock) {
@@ -120,16 +121,18 @@ public class RenderQuarry extends TileEntitySpecialRenderer<TileQuarry> {
                         taskBreakBlock.prevClientPower +
                             (taskBreakBlock.clientPower - taskBreakBlock.prevClientPower) * (double) partialTicks
                     );
-                    yOffset = (double) power / taskBreakBlock.getTarget();
-                    if (yOffset < 0.9) {
-                        yOffset = 1 - yOffset / 0.9;
+                    AxisAlignedBB aabb = tile.getWorld().getBlockState(pos).getBoundingBox(tile.getWorld(), pos);;
+                    double value = (double) power / taskBreakBlock.getTarget();
+                    if (value < 0.9) {
+                        value = 1 - value / 0.9;
                     } else {
-                        yOffset = (yOffset - 0.9) / 0.1;
+                        value = (value - 0.9) / 0.1;
                     }
+                    double scaleMin = 1 - (1 - aabb.maxY) - (aabb.maxY - aabb.minY) / 2;
+                    double scaleMax = 1 + 4 / 16D;
+                    yOffset = scaleMin + value * (scaleMax - scaleMin);
                 }
             }
-
-            yOffset += 4 / 16D;
 
             profiler.endStartSection("frame");
             if (tile.clientDrillPos != null && tile.prevClientDrillPos != null) {
