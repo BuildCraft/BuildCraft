f115373e81af5d1f636448b02430f8d8b03bbf03
Anton Danilkin <afdw@yandex.ru>
2018-03-04 12:18:48 +0300

    Fix #3997

--- common/buildcraft/core/BCCoreModels.java
+++ common/buildcraft/core/BCCoreModels.java
@@ -6,22 +6,21 @@
 
 package buildcraft.core;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
-import com.google.common.collect.ImmutableMap;
-
-import net.minecraft.client.renderer.block.model.BakedQuad;
-import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.registry.IRegistry;
 
 import net.minecraftforge.client.event.ModelBakeEvent;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import buildcraft.api.enums.EnumEngineType;
 import buildcraft.api.enums.EnumPowerStage;
@@ -45,7 +44,6 @@ import buildcraft.core.tile.TileEngineRedstone_BC8;
 import buildcraft.core.tile.TileMarkerVolume;
 
 public class BCCoreModels {
-
     private static final NodeVariableDouble ENGINE_PROGRESS;
     private static final NodeVariableObject<EnumPowerStage> ENGINE_STAGE;
     private static final NodeVariableObject<EnumFacing> ENGINE_FACING;
@@ -59,30 +57,36 @@ public class BCCoreModels {
         ENGINE_STAGE = fnCtx.putVariableObject("stage", EnumPowerStage.class);
         ENGINE_FACING = fnCtx.putVariableObject("direction", EnumFacing.class);
 
-        ENGINE_REDSTONE = getModel("block/engine_redstone.json", fnCtx);
-        ENGINE_CREATIVE = getModel("block/engine_creative.json", fnCtx);
-    }
-
-    private static ModelHolderVariable getModel(String loc, FunctionContext fnCtx) {
-        return new ModelHolderVariable("buildcraftcore:models/" + loc, fnCtx);
+        ENGINE_REDSTONE = new ModelHolderVariable(
+            "buildcraftcore:models/block/engine_redstone.json",
+            fnCtx
+        );
+        ENGINE_CREATIVE = new ModelHolderVariable(
+            "buildcraftcore:models/block/engine_creative.json",
+            fnCtx
+        );
     }
 
     public static void fmlPreInit() {
         MinecraftForge.EVENT_BUS.register(BCCoreModels.class);
     }
 
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public static void onModelRegistry(ModelRegistryEvent event) {
+        if (BCCoreBlocks.engine != null) {
+            ModelLoader.setCustomStateMapper(BCCoreBlocks.engine, b -> Collections.emptyMap());
+        }
+    }
+
     public static void fmlInit() {
         ClientRegistry.bindTileEntitySpecialRenderer(TileMarkerVolume.class, RenderMarkerVolume.INSTANCE);
         ClientRegistry.bindTileEntitySpecialRenderer(TileEngineRedstone_BC8.class, RenderEngineWood.INSTANCE);
         ClientRegistry.bindTileEntitySpecialRenderer(TileEngineCreative.class, RenderEngineCreative.INSTANCE);
-        if (BCCoreBlocks.engine != null) {
-            ModelLoader.setCustomStateMapper(BCCoreBlocks.engine, b -> ImmutableMap.of());
-        }
     }
 
     @SubscribeEvent
     public static void onModelBake(ModelBakeEvent event) {
-        IRegistry<ModelResourceLocation, IBakedModel> modelRegistry = event.getModelRegistry();
         ENGINE_PROGRESS.value = 0.2;
         ENGINE_STAGE.value = EnumPowerStage.BLUE;
         ENGINE_FACING.value = EnumFacing.UP;
@@ -90,32 +94,35 @@ public class BCCoreModels {
         varData.setNodes(ENGINE_REDSTONE.createTickableNodes());
         varData.tick();
         varData.refresh();
-        List<BakedQuad> quads = new ArrayList<>();
-        for (MutableQuad quad : ENGINE_REDSTONE.getCutoutQuads()) {
-            quads.add(quad.toBakedItem());
-        }
-        registerModel(modelRegistry, EnumEngineType.WOOD.getItemModelLocation() + "#inventory",
-            new ModelItemSimple(quads, ModelItemSimple.TRANSFORM_BLOCK, true));
-
-        quads = new ArrayList<>();
+        event.getModelRegistry().putObject(
+            new ModelResourceLocation(EnumEngineType.WOOD.getItemModelLocation(), "inventory"),
+            new ModelItemSimple(
+                Arrays.stream(ENGINE_REDSTONE.getCutoutQuads())
+                    .map(MutableQuad::toBakedItem)
+                    .collect(Collectors.toList()),
+                ModelItemSimple.TRANSFORM_BLOCK,
+                true
+            )
+        );
         ENGINE_STAGE.value = EnumPowerStage.BLACK;
         varData.setNodes(ENGINE_CREATIVE.createTickableNodes());
         varData.tick();
         varData.refresh();
-        for (MutableQuad quad : ENGINE_CREATIVE.getCutoutQuads()) {
-            quads.add(quad.toBakedItem());
-        }
-        registerModel(modelRegistry, EnumEngineType.CREATIVE.getItemModelLocation() + "#inventory",
-            new ModelItemSimple(quads, ModelItemSimple.TRANSFORM_BLOCK, true));
+        event.getModelRegistry().putObject(
+            new ModelResourceLocation(EnumEngineType.CREATIVE.getItemModelLocation(), "inventory"),
+            new ModelItemSimple(
+                Arrays.stream(ENGINE_CREATIVE.getCutoutQuads())
+                    .map(MutableQuad::toBakedItem)
+                    .collect(Collectors.toList()),
+                ModelItemSimple.TRANSFORM_BLOCK,
+                true
+            )
+        );
     }
 
-    private static void registerModel(IRegistry<ModelResourceLocation, IBakedModel> modelRegistry, String reg,
-        IBakedModel val) {
-        modelRegistry.putObject(new ModelResourceLocation(reg), val);
-    }
-
-    private static MutableQuad[] getEngineQuads(ModelHolderVariable model, TileEngineBase_BC8 tile,
-        float partialTicks) {
+    private static MutableQuad[] getEngineQuads(ModelHolderVariable model,
+                                                TileEngineBase_BC8 tile,
+                                                float partialTicks) {
         ENGINE_PROGRESS.value = tile.getProgressClient(partialTicks);
         ENGINE_STAGE.value = tile.getPowerStage();
         ENGINE_FACING.value = tile.getCurrentFacing();
