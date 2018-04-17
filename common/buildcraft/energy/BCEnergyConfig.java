BuildCraft/BuildCraft

commit 4ba5a39a6bbb0c3326e4ffc88b08368eec3ee61b

Ported option to disable generation of oil spout.
Fix #4080.

common/buildcraft/energy/BCEnergyConfig.java
@@ -31,6 +31,7 @@ public class BCEnergyConfig {
 
     public static boolean enableOilGeneration;
     public static double oilWellGenerationRate;
+    public static boolean enableOilSpouts;
     public static final TIntSet excludedDimensions = new TIntHashSet();
     public static final Set<ResourceLocation> excessiveBiomes = new HashSet<>();
     public static final Set<ResourceLocation> surfaceDepositBiomes = new HashSet<>();
@@ -39,6 +40,7 @@ public class BCEnergyConfig {
 
     private static Property propEnableOilGeneration;
     private static Property propOilWellGenerationRate;
+    private static Property propEnableOilSpouts;
     private static Property propExcessiveBiomes;
     private static Property propSurfaceDepositBiomes;
     private static Property propExcludedBiomes;
@@ -56,6 +58,10 @@ public class BCEnergyConfig {
         propOilWellGenerationRate = BCCoreConfig.config.get("worldgen", "oilWellGenerationRate", 1.0);
         propOilWellGenerationRate.setComment("The rate of occurrence of oil wells.");
         game.setTo(propOilWellGenerationRate);
+
+        propEnableOilSpouts = BCCoreConfig.config.get("worldgen", "enableOilSpouts", true);
+        propEnableOilSpouts.setComment("Whether oil spouts are generated or not. The oil spring at the bottom of large lakes will still exist.");
+        game.setTo(propEnableOilSpouts);
 
         String[] _excessive = { //
             BCEnergy.MODID + ":oil_desert", //
@@ -108,6 +114,7 @@ public class BCEnergyConfig {
             if (EnumRestartRequirement.GAME.hasBeenRestarted(restarted)) {
                 enableOilGeneration = propEnableOilGeneration.getBoolean();
                 oilWellGenerationRate = propOilWellGenerationRate.getDouble();
+                enableOilSpouts = propEnableOilSpouts.getBoolean();
                 christmasEventStatus = ConfigUtil.parseEnumForConfig(propChristmasEventType, SpecialEventType.DAY_ONLY);
             } else {
                 validateBiomeNames();

common/buildcraft/energy/generation/OilGenerator.java
@@ -109,13 +109,16 @@ public enum OilGenerator implements IWorldGenerator {
             bonus *= 30.0;
         }
         final GenType type;
-        if (rand.nextDouble() <= CHANCE_LARGE * bonus) {
+
+        final double random_number = rand.nextDouble();
+
+        if (random_number <= CHANCE_LARGE * bonus) {
             // 0.04%
             type = GenType.LARGE;
-        } else if (rand.nextDouble() <= CHANCE_MEDIUM * bonus) {
+        } else if (random_number <= CHANCE_MEDIUM * bonus) {
             // 0.1%
             type = GenType.MEDIUM;
-        } else if (oilBiome && rand.nextDouble() <= CHANCE_LAKE * bonus) {
+        } else if (oilBiome && random_number <= CHANCE_LAKE * bonus) {
             // 2%
             type = GenType.LAKE;
         } else {
@@ -151,16 +154,17 @@ public enum OilGenerator implements IWorldGenerator {
             structures.add(createSphere(new BlockPos(x, wellY, z), radius));
 
             // Generate a spout
-
-            int height;
-            if (type == GenType.LARGE) {
-                height = 9 + rand.nextInt(10);
-                radius = 1;
-            } else {
-                height = 6 + rand.nextInt(7);
-                radius = 0;
-            }
-            structures.add(createSpout(new BlockPos(x, wellY, z), height, radius));
+            if (BCEnergyConfig.enableOilSpouts) {
+                int height;
+                if (type == GenType.LARGE) {
+                    height = 9 + rand.nextInt(10);
+                    radius = 1;
+                } else {
+                    height = 6 + rand.nextInt(7);
+                    radius = 0;
+                }
+                structures.add(createSpout(new BlockPos(x, wellY, z), height, radius));
+            };
 
             // Generate a spring at the very bottom
             if (type == GenType.LARGE) {

commit 906044e041e113aa251593b1bcdfd3e8951a2e00

Fix #4080 - Ported option to disable generation of oil spout and change rate of occurrence of large oil wells (#4130)
* Ported option to change rate of occurence of a oil well.

* Ported option to disable generation of oil spout.

Fix #4080.

* Privatized enum of OilGenerator.

The GenType is only used within a method, and does not need to be
exposed.

* Reverted error introduced in 4ba5a39.

* Added missing issue number in changelog.

common/buildcraft/energy/BCEnergyConfig.java
@@ -30,6 +30,8 @@ import buildcraft.core.BCCoreConfig;
 public class BCEnergyConfig {
 
     public static boolean enableOilGeneration;
+    public static double oilWellGenerationRate;
+    public static boolean enableOilSpouts;
     public static final TIntSet excludedDimensions = new TIntHashSet();
     public static final Set<ResourceLocation> excessiveBiomes = new HashSet<>();
     public static final Set<ResourceLocation> surfaceDepositBiomes = new HashSet<>();
@@ -37,6 +39,8 @@ public class BCEnergyConfig {
     public static SpecialEventType christmasEventStatus = SpecialEventType.DAY_ONLY;
 
     private static Property propEnableOilGeneration;
+    private static Property propOilWellGenerationRate;
+    private static Property propEnableOilSpouts;
     private static Property propExcessiveBiomes;
     private static Property propSurfaceDepositBiomes;
     private static Property propExcludedBiomes;
@@ -48,8 +52,16 @@ public class BCEnergyConfig {
         EnumRestartRequirement game = EnumRestartRequirement.GAME;
 
         propEnableOilGeneration = BCCoreConfig.config.get("worldgen", "enableOilGen", true);
-        propEnableOilGeneration.setComment("Should any oil sprouts or lakes generate, at all?");
+        propEnableOilGeneration.setComment("Should any oil sprouts or lakes be generated at all?");
         game.setTo(propEnableOilGeneration);
+
+        propOilWellGenerationRate = BCCoreConfig.config.get("worldgen", "oilWellGenerationRate", 1.0);
+        propOilWellGenerationRate.setComment("The rate of occurrence of oil wells.");
+        game.setTo(propOilWellGenerationRate);
+
+        propEnableOilSpouts = BCCoreConfig.config.get("worldgen", "enableOilSpouts", true);
+        propEnableOilSpouts.setComment("Whether oil spouts are generated or not. The oil spring at the bottom of large lakes will still exist.");
+        game.setTo(propEnableOilSpouts);
 
         String[] _excessive = { //
             BCEnergy.MODID + ":oil_desert", //
@@ -101,6 +113,8 @@ public class BCEnergyConfig {
 
             if (EnumRestartRequirement.GAME.hasBeenRestarted(restarted)) {
                 enableOilGeneration = propEnableOilGeneration.getBoolean();
+                oilWellGenerationRate = propOilWellGenerationRate.getDouble();
+                enableOilSpouts = propEnableOilSpouts.getBoolean();
                 christmasEventStatus = ConfigUtil.parseEnumForConfig(propChristmasEventType, SpecialEventType.DAY_ONLY);
             } else {
                 validateBiomeNames();

common/buildcraft/energy/generation/OilGenerator.java
@@ -39,7 +39,7 @@ public enum OilGenerator implements IWorldGenerator {
     private static final double CHANCE_MEDIUM = 0.001;
     private static final double CHANCE_LAKE = 0.02;
 
-    public enum GenType {
+    private enum GenType {
         LARGE,
         MEDIUM,
         LAKE,
@@ -104,10 +104,12 @@ public enum OilGenerator implements IWorldGenerator {
         boolean oilBiome = BCEnergyConfig.surfaceDepositBiomes.contains(biome.getRegistryName());
 
         double bonus = oilBiome ? 3.0 : 1.0;
+        bonus *= BCEnergyConfig.oilWellGenerationRate;
         if (BCEnergyConfig.excessiveBiomes.contains(biome.getRegistryName())) {
             bonus *= 30.0;
         }
         final GenType type;
+
         if (rand.nextDouble() <= CHANCE_LARGE * bonus) {
             // 0.04%
             type = GenType.LARGE;
@@ -150,16 +152,17 @@ public enum OilGenerator implements IWorldGenerator {
             structures.add(createSphere(new BlockPos(x, wellY, z), radius));
 
             // Generate a spout
-
-            int height;
-            if (type == GenType.LARGE) {
-                height = 9 + rand.nextInt(10);
-                radius = 1;
-            } else {
-                height = 6 + rand.nextInt(7);
-                radius = 0;
+            if (BCEnergyConfig.enableOilSpouts) {
+                int height;
+                if (type == GenType.LARGE) {
+                    height = 9 + rand.nextInt(10);
+                    radius = 1;
+                } else {
+                    height = 6 + rand.nextInt(7);
+                    radius = 0;
+                }
+                structures.add(createSpout(new BlockPos(x, wellY, z), height, radius));
             }
-            structures.add(createSpout(new BlockPos(x, wellY, z), height, radius));
 
             // Generate a spring at the very bottom
             if (type == GenType.LARGE) {