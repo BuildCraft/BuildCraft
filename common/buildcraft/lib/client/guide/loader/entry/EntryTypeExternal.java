22085b9bac7d86426f8dc461c1f647810f74e9c6
AlexIIL <alexj9@me.com>
2018-05-19 16:55:20 +0100

    Rename "seperate" guide page entries to "external" because that way I won't make the same spelling mistake everytime.
    Thanks @XakepSDK !

--- common/buildcraft/lib/client/guide/loader/entry/EntryTypeExternal.java
+++ common/buildcraft/lib/client/guide/loader/entry/EntryTypeExternal.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 import buildcraft.lib.gui.ISimpleDrawable;
 
-/** A separate page type that doesn't correspond to any object in the game. Used for detailing configs, resource packs,
+/** An external page type that doesn't correspond to any object in the game. Used for detailing configs, resource packs,
  * model files, etc. */
-public class EntryTypeSeperate extends PageEntryType<String> {
+public class EntryTypeExternal extends PageEntryType<String> {
 
-    public static final String ID = "seperate";
-    public static final EntryTypeSeperate INSTANCE = new EntryTypeSeperate();
+    public static final String ID = "external";
+    public static final EntryTypeExternal INSTANCE = new EntryTypeExternal();
 
     @Override
     public String deserialise(String source) {
