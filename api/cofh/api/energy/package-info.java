c09be6f465f62b325076bce674233bf6ac875f7f
asie <kontakt@asie.pl>
2019-04-09 21:59:14 +0200

    update energy API, Gradle wrapper version

--- api/cofh/api/energy/package-info.java
+++ api/cofh/api/energy/package-info.java
@@ -1,11 +1,10 @@
 /**
- * (C) 2015 Team CoFH / CoFH / Cult of the Full Hub
+ * (C) 2014 Team CoFH / CoFH / Cult of the Full Hub
  * http://www.teamcofh.com
  */
 @API(apiVersion = CoFHAPIProps.VERSION, owner = "CoFHAPI", provides = "CoFHAPI|energy")
 package cofh.api.energy;
 
-import cpw.mods.fml.common.API;
-
 import cofh.api.CoFHAPIProps;
+import cpw.mods.fml.common.API;
 
--- api/cofh/api/package-info.java
+++ api/cofh/api/package-info.java
@@ -1,5 +1,5 @@
 /**
- * (C) 2015 Team CoFH / CoFH / Cult of the Full Hub
+ * (C) 2014 Team CoFH / CoFH / Cult of the Full Hub
  * http://www.teamcofh.com
  */
 @API(apiVersion = CoFHAPIProps.VERSION, owner = "CoFHLib", provides = "CoFHAPI")
