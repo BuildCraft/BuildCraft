5904967d9a94bd5de5103a7320663b125e80edb9
Adrian Siekierka <kontakt@asie.pl>
2023-02-12 11:20:38 +0100

    optimize and remove unused PNGs

--- common/buildcraft/core/DefaultProps.java
+++ common/buildcraft/core/DefaultProps.java
@@ -25,7 +25,9 @@ public class DefaultProps {
 	public static String TEXTURE_PATH_BLOCKS = "/gfx/buildcraft/blocks";
 	public static String TEXTURE_PATH_ENTITIES = "/gfx/buildcraft/entities";
 
+	@Deprecated
 	public static String TEXTURE_BLOCKS = TEXTURE_PATH_BLOCKS + "/blocks.png";
+	@Deprecated
 	public static String TEXTURE_ITEMS = "/gfx/buildcraft/items/items.png";
 	public static String TEXTURE_ICONS = TEXTURE_PATH_GUI + "/icons.png";
 	public static String TEXTURE_TRIGGERS = TEXTURE_PATH_GUI + "/triggers.png";
