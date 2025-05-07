566f88e2c73db73ee85979da92c04cd0076a104c
CalenXwX <ddvxyy@sina.cn>
2025-05-07 22:12:14 +0800

    Fix MJ-RF conversion & Make programming recipe input item modifiable

--- common/buildcraft/lib/recipe/programming/ProgrammingRecipeSerializer.java
+++ common/buildcraft/lib/recipe/programming/ProgrammingRecipeSerializer.java
@@ -1,6 +1,7 @@
 package buildcraft.lib.recipe.programming;
 
 import buildcraft.api.recipes.IProgrammingRecipe;
+import buildcraft.api.recipes.IngredientStack;
 import buildcraft.lib.misc.JsonUtil;
 import com.google.gson.JsonObject;
 import net.minecraft.item.ItemStack;
@@ -23,13 +24,15 @@ public class ProgrammingRecipeSerializer extends ForgeRegistryEntry<IRecipeSeria
     @Override
     public IProgrammingRecipe fromJson(ResourceLocation recipeId, JsonObject json) {
         String type = JSONUtils.getAsString(json, "type");
+        IngredientStack input = JsonUtil.deSerializeIngredientStack(json.getAsJsonObject("input"));
         ItemStack output = JsonUtil.deSerializeItemStack(json.getAsJsonObject("output"));
         long energyCost = JSONUtils.getAsLong(json, "energyCost");
-        return new BoardProgrammingRecipe(recipeId, output, energyCost);
+        return new BoardProgrammingRecipe(recipeId, input, output, energyCost);
     }
 
     public static void toJson(ProgrammingRecipeBuilder builder, JsonObject json) {
         json.addProperty("type", IProgrammingRecipe.TYPE_ID.toString());
+        json.add("input", JsonUtil.serializeIngredientStack(builder.getInput()));
         json.add("output", JsonUtil.serializeItemStack(builder.getOutput()));
         json.addProperty("energyCost", builder.getEnergyCost());
     }
@@ -38,14 +41,16 @@ public class ProgrammingRecipeSerializer extends ForgeRegistryEntry<IRecipeSeria
     @Override
     public IProgrammingRecipe fromNetwork(ResourceLocation recipeId, PacketBuffer buffer) {
         ResourceLocation id = buffer.readResourceLocation();
+        IngredientStack input = IngredientStack.fromNetwork(buffer);
         ItemStack output = buffer.readItem();
         long energyCost = buffer.readLong();
-        return new BoardProgrammingRecipe(id, output, energyCost);
+        return new BoardProgrammingRecipe(id, input, output, energyCost);
     }
 
     @Override
     public void toNetwork(PacketBuffer buffer, IProgrammingRecipe recipe) {
         buffer.writeResourceLocation(recipe.getId());
+        recipe.getInput().toNetwork(buffer);
         buffer.writeItem(recipe.getOutput());
         buffer.writeLong(recipe.getEnergyCost());
     }
